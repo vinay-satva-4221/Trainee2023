16c6b2c4d0f429733e8b53f573abeeab06196860
Samiksha mandloi <samiksha140302@gmail.com>
2023-01-27 19:08:21 +0530

    student form task update

--- Trainee2023/Samiksha Mandloi/JS/slabtax.js
+++ Trainee2023/Samiksha Mandloi/JS/slabtax.js
@@ -1,5 +1,16 @@
 //   Birthdate
 var years = "";
+var taxamount;
+var amt;
+var tax3;
+var tax2;
+var taax2;
+var taax3;
+var paybalamt;
+var nonpayamt;
+var loan = document.getElementById("loan").value;
+var income = document.getElementById("income").value;
+var investment = document.getElementById("investment").value;
 $(document).ready(function () {
     $(function () {
         $('input[name="birthday"]').daterangepicker({
@@ -71,43 +82,43 @@ function mobValidate() {
 
 // checkgender
 function checkgendererror() {
-    var valid = true;
-    var e = document.getElementById("gen");
-    
-    var strUser = e.options[e.selectedIndex].value;
-    var strUser1 = e.options[e.selectedIndex].text;
-    var message = "";
-    if (strUser == 0) {
-        message = "Please select a gender";
-        valid = false;
+        var gender = document.getElementById("gen").value;
+        if(gender == " "){
+            document.getElementById("sname").innerHTML = "Enter valid amount of Income";
+        document.getElementById("sname").style.display = "unset";
+        document.getElementById("gen").focus();
+        return false;
     }
-    document.getElementById("sname").innerHTML = message;
-    return valid;
+    else {
+        document.getElementById("sname").style.display = "none";
+        return true;
+    }
+    
 }
 
 // Income
 function checkincomeerror() {
-    let income = document.getElementById("income").value;
+     income = document.getElementById("income").value;
     let regex = /^[0-9]$/;
-    var message = ""
+
     if (income == "") {
         document.getElementById("incm").innerHTML = "Enter valid amount of Income";
         document.getElementById("incm").style.display = "unset";
         document.getElementById("income").focus();
-        valid = false;
+        return false;
     }
     else {
         document.getElementById("incm").style.display = "none";
-        valid = true;
+        return true;
     }
 }
 
 //   loan
 function checkloanerror() {
-    let loan = document.getElementById("loan").value;
+     loan = document.getElementById("loan").value;
     let regex = /^[0-9]$/;
     if (loan == "") {
-        document.getElementById("lon").innerHTML = "Enter valid amount of Income";
+        document.getElementById("lon").innerHTML = "Enter valid amount of loan";
         document.getElementById("lon").style.display = "unset";
         document.getElementById("loan").focus();
         return false;
@@ -119,10 +130,10 @@ function checkloanerror() {
 }
 //   investment
 function checkinvestmenterror() {
-    let income = document.getElementById("investment").value;
+     investment = document.getElementById("investment").value;
     let regex = /^[0-9]$/;
     if (income == "") {
-        document.getElementById("invst").innerHTML = "Enter valid amount of Income";
+        document.getElementById("invst").innerHTML = "Enter valid amount of Investment";
         document.getElementById("invst").style.display = "unset";
         document.getElementById("investment").focus();
         return false;
@@ -136,7 +147,7 @@ function checkinvestmenterror() {
 
 // for Senior citizen
 function seniorCitizen(years) {
-    let valid = false;
+    var valid = false;
     if (years >= 60) {
         valid = true;
     } else {
@@ -147,157 +158,136 @@ function seniorCitizen(years) {
 
 // check validations
 function checkvalidation() {
-    
-    if (!nameValidate()) {
-        document.getElementById("error").innerHTML = "please enter Name ";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
-    } else if (!mobValidate()) {
-        document.getElementById("error").innerHTML = "please enter Mobile numbers";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
-    } else if (!validAge(years)) {
-        document.getElementById("error").innerHTML = "please enter valid age";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
-    } else if (!checkgendererror()) {
-        document.getElementById("error").innerHTML = "please select gender";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
-    } else if (!checkincomeerror()) {
-        document.getElementById("error").innerHTML = "please enter income";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
-    } else if (!checkloanerror()) {
-        document.getElementById("error").innerHTML = "please fill valid loan";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
-    } else if (!checkinvestmenterror()) {
-        document.getElementById("error").innerHTML = "please fill valid investment";
-        document.getElementById("modalname").style.display = "none";
-        document.getElementById("modalamt").style.display = "none";
-        document.getElementById("modaltax").style.display = "none";
+    if (income < loan && income < investment) {
+        alert("Income should be greater");
     } else {
-        //  document.getElementById("stpname") = document.getElementById("name").value;
-          return calculatetax();
-    }
-    if (nameValidate() && mobValidate() && validAge(years) && checkgendererror() && checkincomeerror() && checkloanerror() && checkinvestmenterror()) {
-       return calculatetax();
+        if (!nameValidate()) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else if (!mobValidate()) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else if (!validAge(years)) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else if (!checkgendererror()) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else if (!checkincomeerror()) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else if (!checkloanerror()) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else if (!checkinvestmenterror()) {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("hide");
+            });
+        } else {
+            $(document).ready(function () {
+                $("#exampleModalToggle").modal("show");
+            });  
+            calculatetax();
+            
+        }
     }
 
 }
 
-// for get the value 
-var Name = document.getElementById("name").value;
-var Birth = parseInt(document.getElementById("dob").value);
-var IIncome =document.getElementById("income").value;
-var Loan = document.getElementById("loan").value;
-var Investment = document.getElementById("investment").value;
-var non_payable = loanable();
-
-
-
-
-function payable() {
-    let loan = parseInt(document.getElementById("loan").value);
-    let income = parseInt(document.getElementById("income").value);
-    let incomeper = (income * 20) / 100;
-    let loanper = (loan * 80) / 100;
-    var amt= (Math.min(incomeper, loanper)); 
-    if (parseInt(incomeper) > parseInt(loanper)) {
-        var amt = income - loanper;
-    } else {
-        var amt = income - incomeper ;
-    }
-    return amt;
-}
-function loanable() {
-    let investment = parseInt(document.getElementById("investment").value);
-    if (investment >= 100000) {
-        var txtamt = 100000;
-    }
-    else {
-        var txtamt = investment;
-    }
-    return txtamt;
-}
-
-//  calculate 
 function calculatetax() {
-    var taxamount = IIncome + (parseInt(payable()) + parseInt(loanable()));
-    console.log(taxamount);
-    var senior = seniorCitizen(years);
-    // var man = gender;
-    // var women = gender;
-    
-    // for senior citizen
+    var loan = parseInt(document.getElementById("loan").value);
+    var income = parseInt(document.getElementById("income").value);
+    var Investment = parseInt(document.getElementById("investment").value);
+    var gender = document.getElementById("gen").value;
+    var incomepercent = (income * 20) / 100;
+    var loanpercent = (loan * 80) / 100;
+    if (incomepercent < loanpercent) {
+        if (Investment < 100000) {
+             amt = incomepercent + Investment;
+             taxamount = income - amt;
+        } else {
+             amt = incomepercent + 100000;
+            taxamount = income - amt;
+        }
+    } else {
+        if (Investment < 100000) {
+             amt = loanpercent + Investment;
+            taxamount = income - amt;
+        } else {
+             amt = loanpercent + 100000;
+            taxamount = income - amt;
+            return taxamount;
+        }
+    }
+
+
     if (seniorCitizen(years)) {
         if (taxamount < 300000) {
-            console.log("don't have to pay tax");
+            paybalamt = "don't have to pay tax";
         } else {
-            var nonpayamt = taxamount - 300000;
+             nonpayamt = taxamount - 300000;
             if (nonpayamt > 400000) {
-                var tax3 = nonpayamt - 400000;
-                var tax2 = (400000 * 10) / 100;
-                var taax2 = tax2;
+                 tax3 = nonpayamt - 400000;
+                 tax2 = (400000 * 10) / 100;
+                 taax2 = tax2;
             } else {
                 tax2 = (nonpayamt * 10) / 100;
                 taax2 = tax2;
             }
             if (tax3 <= 0) {
-                var paybalamt = tax3;
-                console.log("Pay amount", paybalamt);
+                paybalamt = tax3;
+               
             } else {
-                var taax3 = (tax3 * 20) / 100;
+                 taax3 = (tax3 * 20) / 100;
                 paybalamt = taax3 + taax2;
-                console.log("pay amount", paybalamt);
+               
             }
         }
     }
-    // for male
-    var gender = document.getElementById("gen").value;
+
+    // if
+    //     // for male
+    //     var gender = document.getElementById("gen").value;
     if (gender == "male") {
-        debugger
+
         if (taxamount < 240000) {
-            console.log("don't have to pay tax");
+            paybalamt= "don't have to pay tax";
         } else {
-            var nonpayamt = taxamount - 240000;
+             nonpayamt = taxamount - 240000;
             if (nonpayamt > 360000) {
-                var tax3 = nonpayamt - 360000;
-                var tax2 = (360000 * 10) / 100;
-                var taax2 = tax2;
+                 tax3 = nonpayamt - 360000;
+                 tax2 = (360000 * 10) / 100;
+                 taax2 = tax2;
             } else {
                 tax2 = (nonpayamt * 10) / 100;
-                taax2 = tax2;
+                paybalamt = tax2;
             }
             if (tax3 <= 0) {
-                  paybalamt = tax3;
-                console.log("Pay amount", paybalamt);
+                paybalamt = tax3;
             } else {
-                var taax3 = (tax3 * 20) / 100;
+                 taax3 = (tax3 * 20) / 100;
                 paybalamt = taax3 + taax2;
-                console.log("pay amount", paybalamt);
 
             }
         }
     } else {
 
-    //    for female
-   
+        //    for female
+
         if (taxamount < 260000) {
-            console.log("don't have to pay tax");
+            paybalamt = "don't have to pay tax";
         } else {
-            var nonpayamt = taxamount - 260000;
+             nonpayamt = taxamount - 260000;
             if (nonpayamt > 440000) {
-                var tax3 = nonpayamt - 440000;
-                var tax2 = (440000 * 10) / 100;
-                var taax2 = tax2;
+                 tax3 = nonpayamt - 440000;
+                 tax2 = (440000 * 10) / 100;
+                 taax2 = tax2;
             } else {
                 tax2 = (nonpayamt * 10) / 100;
                 taax2 = tax2;
@@ -306,16 +296,26 @@ function calculatetax() {
                 paybalamt = tax3;
                 console.log("Pay amount", paybalamt);
             } else {
-                var taax3 = (tax3 * 20) / 100;
-             paybalamt = taax3 + taax2;
+                 taax3 = (tax3 * 20) / 100;
+                paybalamt = taax3 + taax2;
                 console.log("pay amount", paybalamt);
             }
         }
+
     }
-debugger
-     document.getElementById("stpname").innerHTML= document.getElementById("name").value;
-     document.getElementById("stepamt").innerHTML= taxamount;
-     document.getElementById("steptax").innerHTML= paybalamt;
+    if(paybalamt == "don't have to pay tax"){
+            document.getElementById("stpname").innerHTML = document.getElementById("name").value;
+            document.getElementById("stepamt").innerHTML = taxamount;
+            document.getElementById("modalamt").style.backgroundColor = "green";
+            document.getElementById("modaltax").style.backgroundColor = "green";
+            document.getElementById("steptax").innerHTML = paybalamt;    
+}  else {
+    document.getElementById("stpname").innerHTML = document.getElementById("name").value;
+    document.getElementById("stepamt").innerHTML = taxamount;
+    document.getElementById("modalamt").style.backgroundColor = "red";
+    document.getElementById("modaltax").style.backgroundColor = "red";
+    document.getElementById("steptax").innerHTML = paybalamt;
+}
 }
 
 
