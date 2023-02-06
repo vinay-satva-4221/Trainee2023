f7ec563d0c371eec65dd8e8b5df8fdbfdd2445f5
Krinal Patel <krinalp1403@gmail.com>
2023-02-06 17:57:43 +0530

    LIFO FIFO Task Completed

--- Trainee2023/Krinal Patel/Javascript/LIFO_FIFO_Task.js
+++ Trainee2023/Krinal Patel/Javascript/LIFO_FIFO_Task.js
@@ -4,9 +4,6 @@ var studentsArray = [];
 
 function validatecheck(){
     debugger;
-
-
-    
     if ( !validatename() || !validatemobile() || !validatezip()) {
         $('#myModal').modal('show')
   
@@ -17,43 +14,24 @@ function validatecheck(){
                     icon: "warning",
                     button: "Ok"
                 });
-               
-      /////
     
     } else {                       
         $('#myModal').modal('hide')
-
-            return validateallow();      
-            
+            return addData(),hide(),hide1();
 
     }
-
-    
-   
 }
 
+
+function hide(){
     
-
-// function modaloc(){
-//     var isValid = validatecheck()
-
-
-//     if (isValid) {
-//         $('#myModal').modal('hide')
-    
-    
-//     } else {
-//         $('#myModal').modal('show'); 
-    
-//     }
-    
-// }
-
-
-
-
-
-
+    var h = document.getElementById("mytable"); 
+    h.classList.remove("hide");
+}
+function hide1(){
+    var h = document.getElementById("hide1"); 
+    h.classList.remove("hide");
+}
 
 function validatename() {
     let setname = /^[a-zA-Z]+$/;
@@ -104,167 +82,134 @@ function validatezip() {
 }
 
 //Cascading Dropdown 
+// debugger;
+const cityList = [
+    { State: "MadhyaPradesh", CityName: "Indore" },
+    { State: "MadhyaPradesh", CityName: "Bhopal" },
+    { State: 'MadhyaPradesh', CityName: "Sirohi" },
+    { State: 'Gujrat', CityName: "Unjha" },
+    { State: 'Gujrat', CityName: "Mehsana" },
+    { State: 'Gujrat', CityName: "Palanpur" },
+    { State: 'Gujrat', CityName: "Sidhpur" },
+    { State: 'Punjab', CityName: "Ludhiana" },
+    { State: 'Punjab', CityName: "Amritsar" },
+    { State: 'Punjab', CityName: "Patiala" },
+    { State: 'Rajasthan', CityName: "Udaipur" },
+    { State: 'Rajasthan', CityName: "Sirohi" },
+    { State: 'Rajasthan', CityName: "Jaisalmer" }
+]
+// jquery started
+$(document).ready(function () {
+    
 
-const CityData = {
-    "Cities" : [
-        {
-            "StateId": 1,
-            "Id": 1,
-            "Name": "Indore"
-        },
-        {
-            "StateId": 1,
-            "Id": 2,
-            "Name": "Bhopal"
-        },
-        {
-            "StateId": 2,
-            "Id": 3,
-            "Name": "Sirohi"
-        },
-        {
-            "StateId": 2,
-            "Id": 4,
-            "Name": "Udaipur"
-        },
-        {
-            "StateId": 2,
-            "Id": 5,
-            "Name": "Jaisalmer"
-        },
-        {
-            "StateId": 3,
-            "Id": 6,
-            "Name": "Ahmedabad"
-        },
-        {
-            "StateId": 3,
-            "Id": 7,
-            "Name": "Vadodara"
-        },
-        {
-            "StateId": 3,
-            "Id": 8,
-            "Name": "Surat"
-        },
-        {
-            "StateId": 4,
-            "Id": 9,
-            "Name": "Ludhiana"
-        },
-        {
-            "StateId": 4,
-            "Id": 10,
-            "Name": "Amritsar"
-        },
-        {
-            "StateId": 4,
-            "Id": 11,
-            "Name": "Patiala"
-        }
-    ]
-}
+    $("#State").change(function () {
+        $("#City").html("<option selected disabled value=''>Choose...</option>");
+        let citys = cityList.filter(e => e.State == $("#State").val());
 
-const StateData = {
-    "States" : [
-        {
-            "Id" : 1,
-            "Name": "Madhya Pradesh"
-        },
-        {
-            "Id" : 2,
-            "Name": "Rajasthan"
-        },
-        {
-            "Id" : 3,
-            "Name": "Gujarat"
-        },
-        {
-            "Id" : 4,
-            "Name": "Punjab"
-        }
-    ]
-}
-
-
-
-$(document).ready(function(){
-
-    var StateJsonData = JSON.parse(StateData);
-    $.each(StateJsonData.States,function(i,option){
-
-        $("#state").append($('<option></option>').val(option.Id).html(option.Name));
-
-
+        citys.forEach(e => {
+            const option = "<option val='" + e.CityName + "'> " + e.CityName + "</option>";
+            $("#City").append(option);
+        })
     })
-
-    $("#state").change(function(){
-        var CityJsonData = JSON.parse(CityData);
-        $("#city").html('');
-        $.each(CityJsonData.Cities,function(i,option){
-            if($("#state").val() == option.StateId){
-
-
-                $("#city").append($('<option></option>').val(option.Id).html(option.Name));
-
-
-            }
-    })
-
-    });
 });
 
 
+function showData() {
+    var studentDetails;
 
-   
-function validateallow() {
+    if (localStorage.getItem("studentDetails") == null) {
+        studentDetails = [];
+    }
+    else {
+        studentDetails = JSON.parse(localStorage.getItem("studentDetails"));
+    }
 
-    var name = $("#name").val();
-    var phone = $("#phone").val();
-    var state = $("#state").val();
-    var city = $("#city").val();
-    var zip = $("#zip").val();
-   
+    var html = "";
 
-    id ++;
-    var stuObj = {id:id, name: name, phone:phone, state: state, city: city, zip:zip}
+    studentDetails.forEach(function (element, index) {
+        html += "<tr>";
+        html += "<td>" + (index+1)+ "</td>";
+        html += "<td>" + element.name + "</td>";
+        html += "<td>" + element.State + "</td>";
+        html += "<td>" + element.City + "</td>";
+        html += "<td>" + element.zip + "</td>";    
+        
 
-        studentsArray.push(stuObj);
-        localStorage.setItem( 'StudentDetails', JSON.stringify(studentsArray));
-
-    prepareTableCell(id, stuObj.name,stuObj.phone,stuObj.state, stuObj.city,stuObj.zip)
-
-    document.getElementById("name").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("state").value = "";
-    document.getElementById("city").value = "";
-    document.getElementById("zip").value = "";
-
-}
-
-//Table insert data
-function prepareTableCell(index, name, phone, state, city, zip) {
-var index= index
-console.log('index', index)
-var table = document.getElementById("mytable");
-var row = table.insertRow();
-var currentIndex = studentsArray.findIndex(x=> x.id == index)
-
-var srCell = row.insertCell(0);
-var nameCell = row.insertCell(1);
-var phoneCell = row.insertCell(2);
-var stateCell = row.insertCell(7);
-var cityCell = row.insertCell(8);
-var zipCell = row.insertCell(9);
-
-
-srCell.innerHTML = currentIndex+1;
-nameCell.innerHTML=name;
-phoneCell.innerHTML=phone;
-stateCell.innerHTML=state;
-cityCell.innerHTML=city;
-zipCell.innerHTML=zip;
+    document.getElementById("tbody").innerHTML = html;
+    });
 
 
 }
 
+function addData() {
+
+    var name = document.getElementById("name").value
+    var State = document.getElementById("State").value
+    var City = document.getElementById("City").value
+    var zip = document.getElementById("zip").value
+
+    var studentDetails;
+
+    var lifo = document.getElementById("lifo").checked;
+    var fifo = document.getElementById("fifo").checked;
+
+    if (localStorage.getItem("studentDetails") == null) {
+        studentDetails = [];
+    }
+    else {
+        studentDetails = JSON.parse(localStorage.getItem("studentDetails"));
+
+        if(studentDetails.length < 5 && lifo == true){
+            if(studentDetails.length < 5){
+                studentDetails.push({
+                    name: name,
+                    State: State,
+                    City: City,
+                    zip: zip
+                })
+            }
+        }
+        else if(studentDetails.length >= 5 && lifo == true){
+            studentDetails.pop();
+            studentDetails.push({
+                    name: name,
+                    State: State,
+                    City: City,
+                    zip: zip
+            })
+
+        }
+
+        if(studentDetails.length < 5 && fifo == true){
+            if(studentDetails.length < 5){
+                studentDetails.push({
+                    name: name,
+                    State: State,
+                    City: City,
+                    zip: zip
+                })
+            }
+        }
+        else if(studentDetails.length >= 5 && fifo == true){
+            studentDetails.pop();
+            studentDetails.unshift({
+                name: name,
+                    State: State,
+                    City: City,
+                    zip: zip
+            });
+
+        }
+
+    }
+
+    
+    localStorage.setItem("studentDetails", JSON.stringify(studentDetails));
+    showData();
+
+    document.getElementById("name").value="";
+    document.getElementById("State").value="";
+    document.getElementById("City").value="";
+    document.getElementById("zip").value="";
+
+}
