3d610beb826bd8755e0bccea05f3f78a595e96cb
Alok pal <alokpal28797@gmail.com>
2023-02-06 14:21:07 +0530

    Modal task has been completed

--- Trainee2023/Alok Pal/Js/Modal_6/index.js
+++ Trainee2023/Alok Pal/Js/Modal_6/index.js
@@ -1,5 +1,36 @@
-// DropDown Data
+// Form Validation
+function validateForm() {
+    if (
+      !nameV() &&
+      !mobileV() &&
+      !cgpaV() &&
+      !BranchV() &&
+      !zipV()
+    ) {
+      valid = false;
+      sweetA = false;
+    } else {
+      return true;
+    }
+  }
 
+  function nameV() {
+    let regName = /^[A-Za-z]+$/;
+    let name = document.getElementById("fname").value;
+    let vname = document.getElementById("navalid");
+    if (!regName.test(name)) {
+      vname.innerHTML = "**Please enter Alphabets only**";
+      document.getElementById("navalid").style.display = "unset";
+      document.getElementById("fname").focus();
+      return false;
+    } else {
+      document.getElementById("navalid").style.display = "none";
+      return true;
+    }
+  }
+
+
+// DropDown Data
 
 const cityList = [
     { State: 'MadhyaPradesh', CityName: "Indore" },
@@ -16,6 +47,8 @@ const cityList = [
     { State: 'Rajasthan', CityName: "Patiala" }
 ]
 
+
+// jquery started
 $(document).ready(function () {
     $("#State").change(function () {
         $("#City").html("<option selected disabled value=''>Choose...</option>");
@@ -27,3 +60,157 @@ $(document).ready(function () {
         })
     })
 });
+
+
+
+// Js 
+
+function showData() {
+    var peopleList;
+
+    if (localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    }
+    else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    var html = "";
+
+    peopleList.forEach(function (element, index) {
+        html += "<tr class=text-center>";
+        html += "<td>" + (index+1)+ "</td>";
+        html += "<td>" + element.firstName + "</td>";
+        html += "<td>" + element.lastName + "</td>";
+        html += "<td>" + element.state + "</td>";
+        html += "<td>" + element.city + "</td>";
+        html += "<td>" + element.zip + "</td>";
+        html += '<td><button onclick="deleteData( '+ index +' )"  class= "btn btn-danger">Delete</button> <button onclick="updateData(' + index + ')" class= "btn btn-warning m-2">Edit</button></td>';
+        
+        html += "</tr>";
+
+    document.getElementById("root").innerHTML = html;
+    // document.querySelector("crudTable tbody").innerHTML = html;
+    });
+
+
+}
+
+// to load data when the page refreshes
+document.onload = showData();
+
+
+// Add data to local storage
+function addData() {
+
+    // i can put here the validations
+    var firstName = document.getElementById("fname").value
+    var lastName = document.getElementById("lname").value
+    var state = document.getElementById("State").value
+    var city = document.getElementById("City").value
+    var zip = document.getElementById("fzip").value
+
+    var peopleList;
+
+    // Lifo function is called 
+    var lifo = document.getElementById('flifo').checked;
+    var fifo = document.getElementById('ffifo').checked;
+    console.log(fifo, "fifo")
+
+    if (localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    }
+    else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+
+        if(peopleList.length < 5 && lifo == true){
+            console.log("alok")
+            if(peopleList.length < 5){
+                peopleList.push({
+                    firstName: firstName,
+                    lastName: lastName,
+                    state: state,
+                    city: city,
+                    zip: zip,
+                })
+            }
+        }
+        else if(peopleList.length >= 5 && lifo == true){
+            peopleList.pop();
+            peopleList.push({
+                firstName: firstName,
+                lastName: lastName,
+                state: state,
+                city: city,
+                zip: zip,
+            })
+
+        }
+
+        if(peopleList.length < 5 && fifo == true){
+            console.log("alok")
+            if(peopleList.length < 5){
+                peopleList.push({
+                    firstName: firstName,
+                    lastName: lastName,
+                    state: state,
+                    city: city,
+                    zip: zip,
+                })
+            }
+        }
+        else if(peopleList.length >= 5 && fifo == true){
+            peopleList.pop();
+            peopleList.unshift({
+                firstName: firstName,
+                lastName: lastName,
+                state: state,
+                city: city,
+                zip: zip,
+            });
+
+        }
+
+    }
+
+    // peopleList.push({
+    //     firstName: firstName,
+    //     lastName: lastName,
+    //     state: state,
+    //     city: city,
+    //     zip: zip,
+    // });
+
+    localStorage.setItem("peopleList", JSON.stringify(peopleList));
+    showData();
+
+    document.getElementById("fname").value="";
+    document.getElementById("lname").value="";
+    document.getElementById("State").value="";
+    document.getElementById("City").value="";
+    document.getElementById("fzip").value="";
+
+}
+
+
+
+// Delete Data
+
+function deleteData(index){
+    var peopleList;
+    if (localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    }
+    else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    peopleList.splice(index,1);
+    localStorage.setItem("peopleList", JSON.stringify(peopleList));
+    showData();
+}
+
+function lifo(){
+    var lifo = document.getElementById('flifo').checked
+    console.log(lifo ,"lifo")
+}
