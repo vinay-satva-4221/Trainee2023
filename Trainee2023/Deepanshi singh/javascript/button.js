58bf7a94c1ddca9dc6164db9c8212cef7de25cda
deepanshi singh <deepanshisingh200401@gmail.com>
2023-02-10 18:50:18 +0530

    new changes

--- Trainee2023/Deepanshi singh/javascript/button.js
+++ Trainee2023/Deepanshi singh/javascript/button.js
@@ -466,4 +466,18 @@ function validate_card() {
 
 
 }
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
+  if(inputtxt.value.match(phoneno))
+        {
+         return true;
+        }
+      else
+        {
+        alert("message");
+        return false;
+        }
+}
+
 
