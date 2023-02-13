bdaa8898f7286c951ff4c7f6d74f16b4c71efbf6
Mahediali27 <mominm596@gmail.com>
2023-02-13 15:14:21 +0530

    deepanshi's changes

--- Trainee2023/Alok Pal/Js/web-9 form/index.js
+++ Trainee2023/Alok Pal/Js/web-9 form/index.js
@@ -71,7 +71,8 @@ function nameV1() {
     var colName = document.getElementById("colorC");
     var errorN = document.getElementById("error");
     if (nameI == '') {
-        errorN.innerHTML = "This Field is required";
+        errorN.style.display = "block";
+        // errorN.innerHTML = "This field is required";
         colName.style.background = "#ffcccc";
         return false;
     }
@@ -79,7 +80,6 @@ function nameV1() {
         colName.style.background = "white";
         document.getElementById("error").style.display = "none";
         return true;
-
     }
 }
 
