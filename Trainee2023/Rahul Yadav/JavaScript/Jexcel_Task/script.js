eb1ed71ff67c8cf8c8e4b88e5d0e5de724889672
Rahul Yadav <rahuly10111@gmail.com>
2023-02-18 00:07:04 +0530

    commit

--- Trainee2023/Rahul Yadav/JavaScript/Jexcel_Task/script.js
+++ Trainee2023/Rahul Yadav/JavaScript/Jexcel_Task/script.js
@@ -9,7 +9,7 @@ var table = jspreadsheet(document.getElementById('spreadsheet'), {
         { type: 'dropdown', width: '100', title: 'Phase', source: ['Initial Phase', 'Final Phase'] },
         { type: 'dropdown', width: '100', title: 'Status', source: ['pending', 'Completed'] },
         { type: 'number', width: '120', title: 'Logged Hours', mask: 'hh:mm' },
-        { type: 'number', width: '120', title: 'Billable Hours', mask: 'hh:mm' },
+        { type: 'integer', width: '120', title: 'Billable Hours', mask: 'hh:mm' },
         { type: 'text', width: '200', title: 'Notes' },
         { type: 'checkbox', width: '100', title: 'Out of stock' },
         { type: 'url', width: '100', title: 'BC Link' },
@@ -17,89 +17,71 @@ var table = jspreadsheet(document.getElementById('spreadsheet'), {
     ],
 
     updateTable: function (instance, cell, col, row, val, label, cellName) {
+        // console.log('instance',instance)
+        // console.log('cell', cell)
+        // console.log('col', col)
+        console.log('row', row)
+        // console.log('val', val)
+        // console.log('label', label)
+        // console.log('cellName', cellName)
+
+        var r = cellName
+        console.log(r)
 
         var html = "";
-        if(data[0] != ''){
-            
-                data.forEach(function (value, index) {
-                    var a = index + 1;
-                    html += "<tr class=text-center>";
-                    html += "<td>" + a + "</td>";
-                    html += "<td>" + (value)[0] + "</td>";
-                    html += "<td>" + (value)[1] + "</td>";
-                    html += "<td>" + (value)[2] + "</td>";
-                    html += "<td>" +  (value)[3]+ "</td>";
-                    html += "<td>" + (value)[4] + "</td>";
-                    html += "<td>" +(value)[5]  + "</td>";
-                    html += "<td>" + (value)[6] + "</td>";
-                    html += "<td>" + (value)[7] + "</td>";
-                    html += "<td>" +(value)[8] + "</td>";
-                    html += "<td>" + (value)[9]+ "</td>";
-                    
-    
-    
-                    html += "</tr>";
-    
-                    document.getElementById("root").innerHTML = html;
-                });
-        }
-        
+
+
+
+
+
+
+        data.forEach(function (value, index) {
+            if (col == 6 && cell.innerText != '') {
+                console.log("col value",col);
+                var a =index + 1;
+                html += "<tr class=text-center>";
+                html += "<td>" + a + "</td>";
+                html += "<td>" + (value)[0] + "</td>";
+                html += "<td>" + (value)[1] + "</td>";
+                html += "<td>" + (value)[2] + "</td>";
+                html += "<td>" + (value)[3] + "</td>";
+                html += "<td>" + (value)[4] + "</td>";
+                html += "<td>" + (value)[5] + "</td>";
+                html += "<td>" + (value)[6] + "</td>";
+                html += "<td>" + (value)[7] + "</td>";
+                html += "<td>" + (value)[8] + "</td>";
+                html += "<td>" + (value)[9] + "</td>";
+                html += "</tr>";
+
+
+                document.getElementById("root").innerHTML = html;
+            }
+        });
+
+
+
+
+
+
+
+          
+      
+
     }
+    
 
 });
 console.log(data);
-// document.getElementById("table").onclick = function () {
-//     var html = "";
-
-//     data.forEach(function (value, index) {
-//         var a = index + 1;
-//         html += "<tr class=text-center>";
-//         html += "<td>" + a + "</td>";
-//         html += "<td>" + (value)[0] + "</td>";
-//         html += "<td>" + (value)[1] + "</td>";
-//         html += "<td>" + index[2] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-//         html += "<td>" + data[index] + "</td>";
-
-
-//         html += "</tr>";
-
-//         document.getElementById("root").innerHTML = html;
-//     });
-// }
-
-// $(document).ready(function () {
-//     $("#table").click(function(){
-//         var data;
-//         var html = "<table border='1|1'>";
-//     for (var i = 0; i < 5; i++) {
-//         html+="<tr>";
-//         html+="<td>"+data[i]+"</td>";
-//         html+="<td>"+data[i]+"</td>";
-//         html+="<td>"+data[i]+"</td>";
-
-//         html+="</tr>";
-
-//     }
-//     html+="</table>";
-//     $("root").html(html);
-//       });
-
-
-// });
-
+var x=table.getColumnData([3]);
+console.log(x)
 document.getElementById("addrow").onclick = function () {
     var int = document.getElementById('input').value;
     if ((Number(int)) > 0) {
         table.insertRow(Number(int));
         document.getElementById("input").value = "";
     }
+
+    log1()
 }
 
 document.getElementById("deleterow").onclick = function () {
@@ -108,21 +90,15 @@ document.getElementById("deleterow").onclick = function () {
 }
 
 
-// $(document).ready(function () {
-//     $('#example').DataTable({
-//         data: data,
-//         columns: [
-//             { title: 'Project Name' },
-//             { title: 'TimeLog Date' },
-//             { title: 'Phase' },
-//             { title: 'Status' },
-//             { title: 'Logged Hours' },
-//             { title: 'Billable Hours' },
-//             { title: 'Notes' },
-//             { title: 'Out of stock' },
-//             { title: 'BC Link' },
-//             { title: 'BC Description' },
-//         ],
-//     });
-// });
+function log1(col) {
 
+    console.log('v',v)
+
+    var v1 = data[5]
+    console.log('v1', v1)
+
+    var e = data
+    console.log(e)
+}
+
+        // }
