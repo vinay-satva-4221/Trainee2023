b8e2e5ff5cb596c6e0bb7b52d00351c2c63ed89b
Deepanshi singh <deepanshi200401@gmail.com>
2023-02-18 18:42:33 +0530

    calender commit

---
+++ Trainee2023/Deepanshi singh/javascript/add.js
@@ -0,0 +1,13 @@
+function add(){
+    var text=document.getElementById("text").value;
+    console.log(text);
+    var spreadsheet=document.getElementById("spreadsheet");
+    var newrow=spreadsheet.inserRow();
+    // var cell1=newrow.insertCell(0);
+    // var cell1=newrow.insertCell(0);
+    // var cell1=newrow.insertCell(0);
+
+    // var cell1=newrow.insertCell(0);
+
+
+}
