cf4903f453055f068fb5d676ee6eb93c2b89e38c
pratikp151001 <121818515+pratikp151001@users.noreply.github.com>
2023-02-22 07:53:00 +0530

    DataTableAdded

--- Trainee2023/Pratik Patel/JS/JS_Exam/stock.js
+++ Trainee2023/Pratik Patel/JS/JS_Exam/stock.js
@@ -4,7 +4,46 @@ $(document).ready(function () {
   var logedinUser = JSON.parse(localStorage.getItem("LogedinUser"));
   $("#loginUseName").html(logedinUser[0].Name);
   $("#PrifleImage").attr("src", logedinUser[0].Picture);
+  
+  //Table Data
+  var data=[
+    {
+        "name":       "Tiger Nixon",
+        "position":   "System Architect",
+        "salary":     "$3,120",
+        "start_date": "2011/04/25",
+        "office":     "Edinburgh",
+        "extn":       "5421"
+    },
+    {
+        "name":       "Garrett Winters",
+        "position":   "Director",
+        "salary":     "$5,300",
+        "start_date": "2011/07/25",
+        "office":     "Edinburgh",
+        "extn":       "8422"
+    }
+]
 
+  $('#table_div').DataTable( {
+    data: data,
+    columns: [
+        { data: 'name' },
+        { data: 'position' },
+        { data: 'salary' },
+        { data: 'office' }
+    ]
+} );
+  //DateRange Picker
+  $('#birthday').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    minYear: 1901,
+    maxYear: parseInt(moment().format('YYYY'),10)
+  }, function(start, end, label) {
+    var years = moment().diff(start, 'years');
+    alert("You are " + years + " years old!");
+  });
   //Logout
   $("#logout").click(function () {
     debugger;
