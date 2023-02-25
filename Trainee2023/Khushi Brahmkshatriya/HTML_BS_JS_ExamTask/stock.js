15a37430a661d6e339d8f039f5d11d5a64c11bc1
Khushi brahmkshatriya <khushi220201@gmail.com>
2023-02-25 19:46:07 +0530

    stock child data added and design changes

--- Trainee2023/Khushi Brahmkshatriya/HTML_BS_JS_ExamTask/stock.js
+++ Trainee2023/Khushi Brahmkshatriya/HTML_BS_JS_ExamTask/stock.js
@@ -29,52 +29,100 @@ $(document).ready(function () {
 
     let PartData = [];
 
-    function createStackTable()
-    {
+    function createStackTable() {
+        function format(d) {
+
+            let dynamicChildRow = '';
+            if (d.Parts && d.Parts.length > 0) {
+              dynamicChildRow += '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+              dynamicChildRow += '<thead><tr><th>#</th><th>Part Number</th><thOrdered</th><th>Assigned</th><th>Notes</th></tr></thead>';
+              dynamicChildRow += '<tbody>';
+              d.Parts.forEach((partdetail) => {
+                dynamicChildRow += '<tr><td>' + partdetail.index + '</td><td>' + partdetail.partnumber + '</td><td>' + partdetail.ordred + '</td><td>' + partdetail.notes + '</td></tr>';
+              });
+              dynamicChildRow += '</tbody></table>';
+            }
+            return dynamicChildRow;
+          }
+        // function format(d) {
+        //     // `d` is the original data object for the row
+        //     return (
+        //         '<table class="table border"style="padding-left:50px;">' +
+        //         "<thead>" +
+        //         "<th>#</th>"+
+        //         "<th>Part Number</th>"+
+        //         "<th>Ordered</th>"+
+        //         "<th>Assigned</th>"+
+        //         "<th>Notes</th>"+
+        //         "</thead>"+
+        //         "<tdbody>"+
+        //         "<tr>"+
+        //         "<td>"+d.Parts.partnumber +"</td>"+
+        //         "<td>"+d.Parts.ordred +"</td>"+
+        //         "<td>"+d.Parts.partnumber +"</td>"+
+        //         "<td>"+d.Parts.notes +"</td>"+
+        //         "</tr>"+
+        //         "</tbody>"+
+        //         "</table>"
+
+        //     );
+        // }
+
         let localData = localStorage.getItem('NewPartNumber');
         let localArray = JSON.parse(localData);
         console.log(localArray);
-        loggedData = JSON.parse(loggedData);
-        console.log(loggedData);
         //var dataSet = localArray.push(...loggedData)
         console.log(localArray)
-    
+
         var table = $("#example").DataTable({
-    
+
             "order": [],
             "dom": 'rtip',
-        
+            columnDefs: [{
+                "defaultContent": "-",
+                "targets": "_all"
+              }],
             data: localArray,
             bInfo: true,
             columns:
-    
+
                 [
-                    {
-                        className: "dt-control",
-                        orderable: false,
-                        data: null,
-                        defaultContent: "",
-                    },
-                    { data: "StockName", title: "Stock Name" },
+                    
+                    { data: "StockName", title: "Stock Name" ,className: "dt-control", 
+                    orderable: false},
                     { data: "ETADate", title: "ETA Date" },
                     { data: "StockLocation", title: "Stock Location" },
-                    { data: "UserName", title: "Created By" },
+                    { data: "CreatedBy", title: "Created By" },
                     {
                         data: DataTable.render.datetime('MM/DD/YYYY'),
                         keyInput: false, title: "Created Date"
                     },
                     { data: "Action", title: "Action" },
-    
+
                 ],
-    
-    
+
+
         });
-       
+        $("#example tbody").on("click", "td.dt-control", function () {
+            var tr = $(this).closest("tr");
+            var row = table.row(tr);
+
+            if (row.child.isShown()) {
+                // This row is already open - close it
+                row.child.hide();
+                tr.removeClass("shown");
+            } else {
+                // Open this row
+                row.child(format(row.data())).show();
+                tr.addClass("shown");
+            }
+        });
+
     }
-    
+
     $('#AddNewSNum').on("click", function () {
 
-       
+
         // $('#partnumber').val();
         // $('#ordred').val();
         // $('#notes').val();
@@ -82,6 +130,7 @@ $(document).ready(function () {
             swal("Error!", "Please enter all the details", "error");
         }
         else {
+            $("#exampleModal2").hide();
             var invoice = Math.floor(100000 + Math.random() * 900000);
             PartData.push({
                 partnumber: $('#partnumber').val(),
@@ -91,7 +140,7 @@ $(document).ready(function () {
             })
             console.log(PartData);
             //$('#AddNewSNum').model('hide');
-            debugger
+            
             let dynamicTR = "<thead><th>Part Number</th><th>Invoice#</th><th>Ordered</th><th>Notes</th></thead><tbody>";
             for (let i = 0; i < PartData.length; i++) {
                 dynamicTR += "<tr>" + "<td>" + PartData[i].partnumber + "</td>" + "<td>" + PartData[i].invoice + "</td>" + "<td>" + PartData[i].ordred + "</td>" + "<td>" + PartData[i].notes + "</td></tr>"
@@ -103,34 +152,12 @@ $(document).ready(function () {
         }
 
     });
+
     $("#ordred").on("keypress keyup blur", function (event) {
         $(this).val($(this).val().replace(/[^\d].+/, ""));
         if ((event.which < 48 || event.which > 57)) {
             event.preventDefault();
         }
-    });
-    $('input[name="etaDate"]').daterangepicker({
-        singleDatePicker: true,
-        
-        autoUpdateInput: false,
-        locale: {
-            cancelLabel: 'Clear'
-        },
-        maxYear: parseInt(moment().format('YYYY'),10)
-      
-      });
-    
-    $("#etaDate").keypress(function(event) {
-        return ( ( event.keyCode || event.which ) === 9 ? true : false );
-    });
-    $('input[name="etaDate"]').on('apply.daterangepicker', function (ev, picker) {
-        $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
-        $("#date-error").hide();
-        $(".error").removeClass("error");
-    });
-
-    $('input[name="etaDate"]').on('cancel.daterangepicker', function (ev, picker) {
-        $(this).val('');
     });
 
     $("#stockForm").validate({
@@ -178,12 +205,17 @@ $(document).ready(function () {
             }
         }
         else {
+            $("#exampleModal").hide();
             addDataToLocal();
+            
         }
     })
     function addDataToLocal() {
 
-        debugger
+        let loginData = JSON.parse(localStorage.getItem('LoggedInUser'));
+        var createdBy = loginData.find(
+            x => x.UserName);
+
         let localData = localStorage.getItem('NewPartNumber');
 
         if (localData) {
@@ -192,6 +224,7 @@ $(document).ready(function () {
             myId = localArray.map(x => x.id)[myId];
             const obj = {
                 id: myId + 1,
+                CreatedBy: createdBy.UserName,
                 StockName: $('#stockName').val(),
                 ETADate: $('#etaDate').val(),
                 StockStatus: $("input[name='btnradio']:checked").val(),
@@ -206,6 +239,7 @@ $(document).ready(function () {
             const arryObj = [];
             const obj = {
                 id: 1,
+                CreatedBy: createdBy.UserName,
                 StockName: $('#stockName').val(),
                 ETADate: $('#etaDate').val(),
                 StockStatus: $('#StockStatus').val(),
@@ -214,7 +248,7 @@ $(document).ready(function () {
             arryObj.push(obj);
             maxId = localStorage.setItem('NewPartNumber', JSON.stringify(arryObj));
         }
-
+        createStackTable();
     }
 
 })
