ee718b8944c4b57b3236a8531100b593a9af1538
Krinal Patel <krinalp1403@gmail.com>
2023-02-25 19:47:21 +0530

    Stock Dynamic almost completed

--- Trainee2023/Krinal Patel/Javascript/Badeloft/Stock.js
+++ Trainee2023/Krinal Patel/Javascript/Badeloft/Stock.js
@@ -1,5 +1,6 @@
 var partDetails=[]; //global variables
-
+var stockDetails =[];
+var tableData;
 window.onload = (event) => {debugger;
     if (localStorage.getItem("LoginDetails") == null) {
       window.location.replace("Badeloft.html");
@@ -42,14 +43,19 @@ function format(d) {
   
   $(document).ready(function () {
     
-
     $("#addpartno").click(function () {
       $("#innermodal").modal("show");
     });
-    var table = $('#stock').DataTable({
-        data:dataSet,
+
+    // let s = JSON.parse(localStorage.getItem("sto"));
+    
+    // console.log("stockDetails",s);
+
+    tableData = $('#stock').DataTable({
+        data:stockDetails,
         lengthChange: false,  
         info: false,
+        paging:false,
         columns: [
             {
                 className: 'dt-control',
@@ -70,11 +76,20 @@ function format(d) {
         ],
         order: [[1, 'asc']],
     });
+
+    // tableData.row.add([1,1,1,1,1,1,1,1]).draw();
+  //   tableData.row.add([{"sname":       "Tiger Nixon",
+  //   "etadate":   "System Architect",
+  //   "stockLocation":   "System Architect",
+  //   "stkstatus":     "$3,120",
+  //   "createdby": "2011/04/25",
+  //   "cdate":     "Edinburgh",
+  // "action": "t43"}]).draw();
   
     // Add event listener for opening and closing details
     $('#stock tbody').on('click', 'td.dt-control', function () {
         var tr = $(this).closest('tr');
-        var row = table.row(tr);
+        var row = tableData.row(tr);
   
         if (row.child.isShown()) {
             // This row is already open - close it
@@ -87,9 +102,9 @@ function format(d) {
         }
     });
   });
-  var dataSet = [
-    ["","C100", "12/08/2021", "Warehouse", "Kenneth Woodard", "12/08/2021", "Lorem lpsum is simply dummy text",""],
-  ];
+  // var dataSet = [
+  //   // ["","C100", "12/08/2021", "Warehouse", "Kenneth Woodard", "12/08/2021", "Lorem lpsum is simply dummy text",""],
+  // ];
  
 
 // function showpartData() {debugger;
@@ -147,7 +162,7 @@ partDetails.push({
 
 function addstockdata(){
   debugger;
-  var stockDetails =[];
+  // var stockDetails =[];
 console.log("Partdetails in stock function:",partDetails);
 
   //Todo
@@ -170,30 +185,52 @@ console.log("Partdetails in stock function:",partDetails);
   var sname = $("#sname").val();
   var etadate = $("#etadate").val();
   var stkstatus = $("#stkstatus").val();
-  var createdby = $("#username").val();
   // var partno = $("#partno").val();
   // var ordered = $("#ordered").val();
   // var notes = $("#notes").val();
+  //Getting username for created by
+  var par = JSON.parse(localStorage.getItem('LoginDetails'));
+  var createdby = par[0].username;
+  
+  //Getting current date
+  var cdate = new Date(Date.now()).toLocaleString().split(',')[0] ;
+console.log(partDetails)
+  var notes = partDetails[0].notes;
 
 
   stockDetails.push({
                 sname: sname,
                 etadate: etadate,
                 stkstatus: stkstatus,   
-                createdby: createdby,             
+                createdby: createdby,  
+                cdate: cdate,    
+                notes:notes,       
   });
 
+  // tableData.row.add({
+  //   sname: sname,
+  //   etadate: etadate,
+  //   stkstatus: stkstatus,   
+  //   createdby: createdby,  
+  //   cdate: cdate,    
+  //   notes:notes}).draw();
+  tableData.row.add( ['',sname,etadate,stkstatus,createdby,cdate,notes,''] ).draw();
   var stockList = [
     {
       "sname" : sname,
       "etadate" : etadate,
       "stkstatus" : stkstatus,
-      "createdby" : createdby,
-      "partlist" :[{
-        partDetails
-      }]
+      "createdby" : cdate,
+      "notes":notes,
+      // "partlist" :[{
+      //   partDetails
+      // }],
+      // "stocklist" : stockDetails,
+      "partlist" : partDetails,
+
     }
   ]
+  console.log("stock array",stockDetails)
 
  console.log("SList",stockList)
 
@@ -201,22 +238,23 @@ console.log("Partdetails in stock function:",partDetails);
   
   var stockList = JSON.parse(localStorage.getItem("stockList"));
 
-var html = "";
+// var html = "";
 
-stockDetails.forEach(function (element, index) {
-      html += "<tr>";
-      html += "<td>" + "" + "</td>";
-      html += "<td>" + element.sname + "</td>";
-      html += "<td>" + element.etadate + "</td>";
-      html += "<td>" + element.stkstatus + "</td>";
-      html += "<td>" + element.createdby + "</td>";
-      html += "<td>" + element.ordered + "</td>";
-      html += "<td>" + element.notes + "</td>";
-      html += "</tr>";
+// stockDetails.forEach(function (element, index) {
+//       html += "<tr>";
+//       html += "<td>" + "" + "</td>";
+//       html += "<td>" + element.sname + "</td>";
+//       html += "<td>" + element.etadate + "</td>";
+//       html += "<td>" + element.stkstatus + "</td>";
+//       html += "<td>" + element.createdby + "</td>";
+//       html += "<td>" + element.cdate + "</td>";
+//       html += "<td>" + element.notes + "</td>";
+//       html += "</tr>";
    
+//       document.getElementById("maintdata").innerHTML = html;
 
-  document.getElementById("maintdata").innerHTML =document.getElementById("maintdata").innerHTML+ html;
-  });
+  // document.getElementById("maintdata").innerHTML =document.getElementById("maintdata").innerHTML+ html;
+  // });
 
   
   document.getElementById("sname").value="";
