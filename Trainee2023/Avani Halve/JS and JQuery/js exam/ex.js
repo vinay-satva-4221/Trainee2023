1d8e74dd186ec935a905b2e404e1a6d39130cefb
avani halve <avani231098@gmail.com>
2023-02-25 19:41:09 +0530

    badeloft changes

--- Trainee2023/Avani Halve/JS and JQuery/js exam/ex.js
+++ Trainee2023/Avani Halve/JS and JQuery/js exam/ex.js
@@ -1,5 +1,5 @@
-var loginUser = JSON.parse(localStorage.getItem('loginUser'));
-var username = loginUser[0].name;
+var loginUser = JSON.parse(localStorage.getItem('loginuser'));
+var username = loginUser.name;
 document.getElementById("user").innerHTML = username;
 
 
@@ -10,24 +10,25 @@ var order = document.getElementById("nporder_no").value;
 var notes = document.getElementById("nnotes_no").value;
 
 function logout() {
-  localStorage.removeItem("loginUser");
-  location.replace("login.html");
+  localStorage.removeItem("loginuser");
+  location.replace("badeloft.html");
 }
 
 function format(d) {
+  
   let childRowHTML = '';
   if (d.itemDetails && d.itemDetails.length > 0) {
-    childRowHTML += '<table cellpadding="5" cellspacing="0" border="0" style="width:100%;">';
-    childRowHTML += '<thead><tr><th>Part No</th><th>Order No</th><th>Notes</th><th>Action</th></tr></thead>';
+    childRowHTML += '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+    childRowHTML += '<thead><tr><th>Part No</th><th>Order No</th><th>Notes</th></tr></thead>';
     childRowHTML += '<tbody>';
     d.itemDetails.forEach((itemDetail) => {
-      childRowHTML += '<tr><td>' + itemDetail.partno + '</td><td>' + itemDetail.order + '</td><td>' + itemDetail.notes + '</td><td><button type="button" class="fa fa-trash"></button></td></tr>';
+      childRowHTML += '<tr><td>' + itemDetail.partno + '</td><td>' + itemDetail.order + '</td><td>' + itemDetail.notes + '</td></tr>';
     });
     childRowHTML += '</tbody></table>';
   }
   return childRowHTML;
 }
-
+// get data from localStorage
 var stockDetails = JSON.parse(localStorage.getItem('stockDetail'));
 
 var table = $("#example").DataTable({
@@ -45,48 +46,38 @@ var table = $("#example").DataTable({
     { data: "createdBy" },
     { data: "createdDate" },
     { data: "notes" },
-    { data: "null",
+    { data: null,
       render: function (data, type, row) {
-      return '<button type="button" class="fa fa-pencil" style="border: none;"></button>' +
-          '<button type="button" class="fa fa-trash" style="border: none;"></button>'; }
-  } 
+          // Return HTML for two buttons
+          return '<button class="btn btn-primary btn-sm">Edit<i class="fa-sharp fa-solid fa-pen"></i></button>' +
+              '<button class="btn btn-danger btn-sm">Delete</button>'; }
+      }
   ],
   order: [[1, "asc"]],
 });
 
-//delete the row
-$('#example tbody').on('click', '.fa-trash', function () {
-  var row = table.row($(this).parents('tr'));
-  var data = row.data();
-  var index = stockDetails.findIndex(function(item) {
-      return item.stockName === data.stockName;
-  });
-  if (index !== -1) {
-      stockDetails.splice(index, 1);
-      localStorage.setItem('stockDetail', JSON.stringify(stockDetails));
-  }
-  row.remove().draw();
-});
-
-
+// Add event listener for opening and closing details
 $("#example tbody").on("click", "td.dt-control", function () {
   var tr = $(this).closest("tr");
   var row = table.row(tr);
+
   if (row.child.isShown()) {
+    // This row is already open - close it
     row.child.hide();
     tr.removeClass("shown");
   } else {
+    // Open this row
     row.child(format(row.data())).show();
     tr.addClass("shown");
   }
 });
 
+// Function to add stock to localStorage and DataTable
 function addStock() {
   debugger
   let stockName = document.getElementById("stock_name").value;
   let eta = document.getElementById("eta_date").value;
   let status = document.querySelector('input[name="stock_status"]:checked').value;
-  //let notes = document.getElementById("nnotes_no").value;
 
   let stockDetails = {
     stockName: stockName,
@@ -94,11 +85,11 @@ function addStock() {
     status: status,
     createdBy: username,
     createdDate: eta,
-    notes: "Static notes",
+    notes: "Hello EveryOne",
     Action:  "",
     itemDetails: stockItemDetails
   };
-
+  
   var stockDetailsArray = JSON.parse(localStorage.getItem('stockDetail'));
   if(!stockDetailsArray){
      stockDetailsArray = [];
@@ -112,9 +103,10 @@ function addStock() {
   document.getElementById("eta_date").value = "";
   stockItemDetails = [];
 }
-
 var stockItemDetails = [];
+
 function addItemDetails() {
+  // debugger;
   let partNo = document.getElementById("npart_no").value;
   let order = document.getElementById("nporder_no").value;
   let notes = document.getElementById("nnotes_no").value;
@@ -127,17 +119,17 @@ function addItemDetails() {
   };
 
   stockItemDetails.push(stockDetail11);
-  
+
   let dtr = "<tr>";
-  dtr = dtr + "<td class='txtpartno' data-id='" + stockDetail11.partno + "' >" + stockDetail11.partno + "</td>";
-  dtr = dtr + "<td class='txtinvoice' >" + (invoice++) + "</td>";
-  dtr = dtr + "<td class='txtorder' >" + stockDetail11.order + "</td>";
-  dtr = dtr + "<td class='txtnotes' >" + stockDetail11.notes + "</td>";
-  dtr = dtr + "<td class='tdAction'><button type='button' class='btn btn-sm btn-delete'>&#x2715;</button></td>";
+  dtr = dtr + "<td class='txtpartno' data-id= " + stockDetail11.partno + " >" + stockDetail11.partno + "</td>";
+  dtr = dtr + "<td class='txtinvoice' >" + (invoice++) +  "</td>";
+  dtr = dtr + "<td class='txtorder' >" + stockDetail11.order +  "</td>";
+  dtr = dtr + "<td class='txtnotes' >" + stockDetail11.notes +  "</td>";
+  dtr = dtr + "<td class='tdAction'><button type='button' class'=btn btn-sm btn-dark btn-delete'>&#x2715;</button></td>";
   dtr = dtr + "</tr>";
   $("#parttable tbody").append(dtr);
+  // invoice++;
 }
-
 $("#parttable tbody").on("click", ".btn-delete", function () {
-  $(this).closest("tr").remove();
+  $(this).closest("tr").remove(); 
 });
