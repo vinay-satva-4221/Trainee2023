a70e37525b5892112b875c7c7cb6014799308c8a
mahediali momin <mahedialimomin17@gmail.com>
2023-02-27 20:12:37 +0530

    all today's changes done

---
+++ Trainee2023/Mahediali Momin/JAVASCRIPT/JAVASCRIPT_EXAM/BadeLoft_Eric_Day7/Dashboard.js
@@ -0,0 +1,93 @@
+// $(document).ready(function () {
+//     var name = localStorage.getItem("details");
+//     console.log(name)
+
+//     var det = name.uname;
+//     console.log(det)
+// })
+
+
+window.onload = () => {
+    if (localStorage.getItem("loggUser") == null) {
+        
+      window.location.replace("LoginPage.html");
+    }
+  }
+
+var data1 = [
+    // ["Stock Location", "", "", "On water", "On water", "In Production"],
+    // ["ETA Date", "", "", "10/08/2021", "10/08/2021", "10/08/2021"],
+    // ["BW-01-S-M", "1", "0", "3", "0", "0"],
+    // ["BW-03-XL-G", "1", "1", "2", "2", "1"],
+    // ["BW-01-Q-M", "", "0", "3", "0", "1"],
+    // ["BW-03-XL-G", "1", "1", "2", "2", "1"],
+    // ["ZK-08-X2P", "1", "0", "3", "0", "1"],
+    // <a data-toggle="popover" title="Assigned to" data-content="Description" data-trigger="hover">
+    // popover</a>
+    ["Stock Location", "", "", "On Water", "On Water", "In production"],
+    ['Eta Date','','','10/08/2021','10/08/2021','10/08/2021'],
+    ['BW-01-S-M','1','0','3','0','0'],
+    ['BW-03-XL-G','1','1','2','2','1'],
+    ['BW-01-Q-M','','0','3','0','1'],
+    ["Stock Location", "", "", "On Water", "On Water", "In production"],
+    ['Eta Date','','','10/08/2021','10/08/2021','10/08/2021'],
+    ['BW-01-S-M','1','0','3','0','0'],
+    ['BW-03-XL-G','1','1','2','2','1'],
+    ['BW-01-Q-M','','0','3','0','1'],
+    ["Stock Location", "", "", "On Water", "On Water", "In production"],
+    ['Eta Date','','','10/08/2021','10/08/2021','10/08/2021'],
+    ['BW-01-S-M','1','0','3','0','0'],
+    ['BW-03-XL-G','1','1','2','2','1'],
+    ['BW-01-Q-M','','0','3','0','1'],
+]
+
+$(document).ready(function () {
+    table = $('#example').DataTable({
+        data: data1,
+        columns: [
+            { title: 'Part Number',  "sortable": true,},
+            { title: 'In Warehouse',  "sortable": false },
+            { title: 'Available',  "sortable": false },
+            { title: 'C100' ,"sortable": false, fnDrawCallback : function() {
+                $('[data-toggle="popover"]').popover(); 
+            }},
+            { title: 'C101',"sortable": false },
+            { title: 'C102', "sortable": false },
+        ],
+        lengthMenu: [
+            [5]
+            [5]
+        ],
+        
+        columnDefs: [
+            {
+                targets: [1,2,3,4,5],
+                className: 'text-center'
+            },
+            {
+                "width": "34%" , "targets": 0
+            },
+           
+          ],
+        //   language: {
+        //     searchPlaceholder: "Search records",
+        //     search: "",
+        //   },
+    });
+    $('#myCustomSearchBox').keyup(function() {
+        table.search($(this).val()).draw(); // this  is for customized searchbox with datatable search feature.
+    })
+
+    $('.sorting').removeClass('sorting');
+
+    var user = JSON.parse(localStorage.getItem("loggUser"));
+    console.log("user",user);
+    $("#Uname").html(user[0].name);
+
+});
+
+
+function logout() {
+    window.location.replace("LoginPage.html");
+    localStorage.clear();
+}
