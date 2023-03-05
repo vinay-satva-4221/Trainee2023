f8316daaf14f41fdb112d44dd8f188066faf7b7b
khushi220201 <khushi220201@gmail.com>
2023-03-05 23:01:41 +0530

    Assignment edit delete and stock delete added

--- Trainee2023/Khushi Brahmkshatriya/HTML_BS_JS_ExamTask/assignment.js
+++ Trainee2023/Khushi Brahmkshatriya/HTML_BS_JS_ExamTask/assignment.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var currentdate = new Date();
+    var CreatedDate = (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear();
     $("#AddNavbar").load("./navbar.html");
     var loggedData = localStorage.getItem('LoggedInUser');
     let loginData = JSON.parse(loggedData);
@@ -12,6 +14,7 @@ $(document).ready(function () {
     else {
         window.location.replace("./login.html");
     }
+    var isEdit = false;
     var loggedData = localStorage.getItem('LoggedInUser');
     if (loggedData) {
         var customerInvoice = [
@@ -61,7 +64,9 @@ $(document).ready(function () {
 
         });
 
-        $('.partnumbers').select2();
+        $('.partnumbers').select2({
+            placeholder: "Select a Part",
+        });
     }
     else {
         window.location.replace("./login.html");
@@ -69,34 +74,40 @@ $(document).ready(function () {
 
     $('#addPartsToStock').click(function () {
 
-        console.log($(".partnumbers").val());
-        var allparts = ($(".partnumbers").val());
-        var partnum = allparts.toString().replaceAll(',','|');
-        partsAssignedToStock.push({
-            StockName: $('#stockname').val(),
-            PartNumbers: partnum,
-            //PartNumbers: ($(".partnumbers").val()),
-
-        })
-        console.log(partsAssignedToStock);
-
-
-        let dynamicTR = "<thead><th>#</th><th>Stock</th><th>Parts</th><th class='text-end'>Action</th></thead><tbody>";
-        for (let i = 0; i < partsAssignedToStock.length; i++) {
-            dynamicTR += "<tr>" + "<td>" + i + "</td>" + "<td>" + partsAssignedToStock[i].StockName + "</td>" + "<td>" + partsAssignedToStock[i].PartNumbers
-                + "</td>" +
-                "<td class='text-end'><button type='button' class='btn-close DeleteParts' aria-label='Close' ></button></td></tr>"
+        if ($(".partnumbers").val() == null || $(".partnumbers").val() == "") {
+            alert('Please select parts')
         }
-        dynamicTR += "</tbody>";
-        console.log(dynamicTR)
-        $('#partTable').html(dynamicTR);
+        else {
 
+
+            console.log($(".partnumbers").val());
+            var allparts = ($(".partnumbers").val());
+            var partnum = allparts.toString().replaceAll(',', '|');
+            partsAssignedToStock.push({
+                StockName: $('#stockname').val(),
+                PartNumbers: partnum,
+                //PartNumbers: ($(".partnumbers").val()),
+
+            })
+            console.log(partsAssignedToStock);
+
+
+            let dynamicTR = "<thead><th>#</th><th>Stock</th><th>Parts</th><th class='text-end'>Action</th></thead><tbody>";
+            for (let i = 0; i < partsAssignedToStock.length; i++) {
+
+                dynamicTR += "<tr>" + "<td>" + i + "</td>" + "<td>" + partsAssignedToStock[i].StockName + "</td>" + "<td>" + partsAssignedToStock[i].PartNumbers
+                    + "</td>" +
+                    "<td class='text-end'><button type='button' class='btn-close DeletePartsTable'  aria-label='Close' ></button></td></tr>"
+            }
+            dynamicTR += "</tbody>";
+            console.log(dynamicTR)
+            $('#partTable').html(dynamicTR);
+        }
     })
     function AddassignmentdataToLocal() {
 
         let localData = localStorage.getItem('Assignment');
-        var currentdate = new Date();
-        var CreatedDate = (currentdate.getMonth()+1)+"/"+currentdate.getDate()+"/"+currentdate.getFullYear();
+
         //var CreatedDate = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate();
         if (localData) {
             let localArray = JSON.parse(localData);
@@ -121,8 +132,8 @@ $(document).ready(function () {
 
                 CreatedBy: createdBy.UserName,
                 Name: $('#customer').val(),
-                CreatedDate:  new Date(),
-                QBInvoice:  $('#quickbookInvoice').val(),
+                CreatedDate: CreatedDate,
+                QBInvoice: $('#quickbookInvoice').val(),
                 PartData: partsAssignedToStock,
             };
             arryObj.push(obj);
@@ -175,14 +186,19 @@ $(document).ready(function () {
                     },
                     {
                         data: "null", title: "Action", className: "dt-center ",
-                        defaultContent: '<i class="bi bi-pencil-fill text-secondary fw-bolder edit stockeditbtn fs-6"/> <i class="bi bi-trash3-fill text-secondary fw-bolder fs-6"/>',
+                        render: function (data, type, row) {
+                            return (
+                                '<button type="button" class="bi bi-pencil-fill text-secondary fw-bolder edit fs-6 border-0 bg-light"></button>' +
+                                '<button type="button" class="bi bi-trash3-fill text-secondary fw-bolder fs-6 border-0 bg-light delete"></button>'
+                            );
+                        },
                     },
                 ],
 
         });
     }
     function format(d) {
-        
+
         int_rownumber = 1;
         let dynamicChildRow = '';
         if (d.PartData && d.PartData.length > 0) {
@@ -192,8 +208,8 @@ $(document).ready(function () {
 
             d.PartData.forEach((partdetail, index) => {
                 let i = index + 1;
-                dynamicChildRow += '<tr><td>' + i + '</td><td>' + partdetail.StockName + '</td><td>' + partdetail.PartNumbers + '</td>'+
-                    '<td>' + `<button type="button" class="btn-close float-end" aria-label="Close"></button>` + '</td>'
+                dynamicChildRow += '<tr><td>' + i + '</td><td>' + partdetail.StockName + '</td><td>' + partdetail.PartNumbers + '</td>' +
+                    '<td>' + `<button type='button' class='btn-close' aria-label='Close' id='childTabledeleteBtn' data-assignment-id='${d.QBInvoice}' onclick='deleteMainTableRow(this)'  data-part-index='${index}' ></button>` + '</td>'
                 '</tr>';
             });
 
@@ -218,40 +234,180 @@ $(document).ready(function () {
     $('#txtSearch').keyup(function () {
         table.search($(this).val()).draw();
         // this  is for customized searchbox with datatable search feature.
-      })
+    })
     const input = document.querySelector('input[type="search"]');
     input.addEventListener("search", () => {
         table.search(input.value).draw();
         // console.log(`The term searched for was ${}`);
     });
+
+    //delete data of partTable
+    $("#partTable").on("click", ".DeletePartsTable", function () {
+        console.log($(this).closest("tr").remove());
+        partsAssignedToStock.splice(this, 1)
+    });
+
+    //delete maintable row
+    $('.delete').click(function () {
+        var AssignedData = JSON.parse(localStorage.getItem("Assignment"));
+        let Index = table.row($(this).parents('tr')).index();
+        table.row(Index).remove().draw()
+        AssignedData.splice(Index, 1)
+        localStorage.setItem("Assignment", JSON.stringify(AssignedData));
+
+    });
+    //editing Assignment data 
+    $('#assignedparttable tbody').on('click', '.edit', function () {
+        isEdit = true;
+        console.log((table.row(this).data()));
+        var data = table.row($(this).parents('tr')).data();
+        var index = table.row($(this).parents('tr')).index();
+
+        // Populate assignmentmodal with data
+        $("#customer").val(data.Name).change();
+        $("#quickbookInvoice").val(data.QBInvoice);
+        // $("#stockname").val(data.StockName).change();
+        // $("#parts").val(data.PartNumbers);
+
+        if (data.PartData && data.PartData.length > 0) {
+
+            let dynamicTR = "<thead><th>#</th><th>Stock</th><th>Part</th><th class='text-end'>Action</th></thead><tbody>";
+            data.PartData.forEach(function (part, index) {
+                var i = index + 1;
+                dynamicTR += ("<tr>" + "<td>" + i + "</td>" + "<td>" + part.StockName + "</td>" + "<td>" + part.PartNumbers + "</td>" +
+                    "<td class='text-end'><button type='button' class='btn-close DeletePartsTable' aria-label='Close'></button></td></tr>");
+            });
+            dynamicTR += "</tbody>";
+            console.log(dynamicTR)
+            $('#partTable').html(dynamicTR);
+
+        }
+
+
+        partsAssignedToStock = data.PartData;
+        console.log(partsAssignedToStock)
+        $(' #assigntitle').html("Edit Assignment");
+        // Show AddStockModal
+        $('#assignmentModal').modal('show');
+        //Handle click on Save Changes button
+        $("#saveAssignments").click(function () {
+
+            var updatedCustomer = $("#customer").val();
+            var updatedInvoice = $("#quickbookInvoice").val();
+            // var updatedStockName = $("#stockname").val();
+
+            // var updatedpart =  $("#parts").val();
+            //var updatedcreateddate = Date.now();
+
+            var assignDataObject = {
+                CreatedBy: createdBy.UserName,
+                Name: updatedCustomer,
+                CreatedDate: CreatedDate,
+                QBInvoice: updatedInvoice,
+                PartData: partsAssignedToStock,
+            };
+            var assignData = JSON.parse(localStorage.getItem("Assignment")) || [];
+            assignData[index] = assignDataObject;
+            console.log(JSON.parse(localStorage.getItem("Assignment")));
+            localStorage.setItem("Assignment", JSON.stringify(assignData));
+            $("#assignmentModal").modal("hide");
+
+        });
+
+    });
+    $("#AssignmentForm").validate({
+        // in 'rules' user have to specify all the constraints for respective fields
+
+        rules: {
+            customer: {
+                required: true,
+
+            },
+            quickbookInvoice: {
+                required: true,
+
+            },
+            stockname: {
+                required: true,
+            },
+
+        },
+        messages: {
+            customer: {
+                required: "Please select customer name",
+
+            },
+            quickbookInvoice: {
+                required: "Please select invoice",
+
+            },
+            stockname: {
+                required: "Please select stock name",
+            }
+        }
+    });
+    var form = $("#AssignmentForm");
+    form.validate();
+
+
     $('#saveAssignments').click(function () {
+        if (!isEdit) {
+            var result = form.valid();
+            console.log(result);
+
+            if (result == true) {
+
+                $('#assignmentModal').modal("hide");
 
 
-        
+                AddassignmentdataToLocal();
+                //resetForm();
 
-        $('#assignmentModal').modal("hide");
-        // $('#stockModal').on('hidden.bs.modal', function () {
-        //     $('.modal-backdrop').remove();
-        // })
+            }
+            else {
+                $('#assignmentModal').modal("show");
+            }
 
-
-        AddassignmentdataToLocal();
-        //resetForm();
+        }
 
 
 
     })
-    $('#newAssignment').click(function(){
-        resetFormDate();
+    $('#newAssignment').click(function () {
+        //resetFormDate();
     })
-    function resetFormDate(){
+    function resetFormDate() {
         $('#customer').val('');
         $('#quickbookInvoice').val('');
         $('#stockname').val('');
         $('#parts').val('');
-      
+
         $('#partTable').html('');
 
 
     }
-});  
+});
+
+// delete partdata from child row
+function deleteMainTableRow(element) {
+    debugger
+    var newStockModal = JSON.parse(localStorage.getItem("Assignment"));
+
+    console.log(newStockModal)
+    var assignID = $(element).attr('data-assignment-id');
+    var assignIndex = newStockModal.findIndex(x => x.QBInvoice == assignID);
+    var PartIndex = $(element).attr('data-part-index');
+    console.log("Part index", PartIndex);
+    console.log(newStockModal[PartIndex]);
+    console.log("Stock index", assignIndex);
+    newStockModal[assignIndex].PartData.splice(PartIndex, 1);
+
+    console.log(newStockModal);
+
+    localStorage.setItem("Assignment", JSON.stringify(newStockModal));
+
+    var tr = $(element).closest("tr")
+    tr.remove()
+
+}
+
