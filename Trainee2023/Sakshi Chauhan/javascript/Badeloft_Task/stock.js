2cfdeab88ea438bd1ce33cd50a05a2afc8ba504c
sakshi chauhan <sakshic0708@gmail.com>
2023-03-06 20:03:15 +0530

    Stock Page Edit & update Done

--- Trainee2023/Sakshi Chauhan/javascript/Badeloft_Task/stock.js
+++ Trainee2023/Sakshi Chauhan/javascript/Badeloft_Task/stock.js
@@ -1,263 +1,406 @@
-var SecondPartDetail = [];
-var StockDetails = [];
-var table;
-var index = 1;
+var partDetails = []; //global variables
+var tableData;
+var stockList = [];
+var todayDate;
+var datatableIndex;
+
 function format(d) {
   console.log(d);
-  // var showPartDetails = d.SecondPartDetail;
-  console.log(SecondPartDetail);
+  // `d` is the original data object for the row
+ 
 
-  if(d.StockDetails==null)
-  {
-    d.StockDetails=SecondPartDetail;
-  }
 
-  var childrow = "";
-  childrow += '<table cellpadding="2" id="ChildTableStock" cellspacing="0" class="table ChildTableStock">';
-  childrow += '<thead><tr><th text-right style="width: 50px">#</th><th style="width: 180px";>Part Number</th><th style="width: 180px;">Ordered</th><th style="width: 180px;">Assigned</th><th style="width: 150px;">Notes</th><th text-left">Action</th></tr></thead>';
+  var stocks = JSON.parse(localStorage.getItem("stockList"));
+  var currentPartDeatils = stocks.find((x) => x.sname == d.sname).partDetails;
+
+
+  let childrow = "";
+
+  childrow += '<table cellpadding="2" cellspacing="0" class="table border rounded">';
+  childrow += '<thead><tr><th>#</th><th>Part Number</th><th>Ordered</th><th>Assigned</th><th>Action</th></tr></thead>';
   childrow += '<tbody>';
-  SecondPartDetail.forEach((e, index) => {
-    var index = (index + 1);
-    childrow += '<tr><td>' + index + '</td><td>' + e.Partnumber + '</td><td>' + e.Ordered + '</td><td>' + 5 + '</td><td>' + e.Notes + '</td><td><h5><i class="fa fa-times"></i></h5></td></tr>';
+  currentPartDeatils.forEach((e, index) => {
+
+    let ind = (index + 1);
+
+    childrow += '<tr><td>' + ind + '</td><td>' + e.partno + '</td><td>' + e.ordered + '</td><td>' + Math.floor(Math.random() * 10) + '</td><td>'+"<button type='button' class='btn-close delete' aria-label='Close' ></button>"+'</td>'+'</tr>';
   });
   childrow += '</tbody></table>';
+  
   return childrow;
 }
 
 
-function logout() {
-  window.location.replace("login.html")
-  localStorage.clear();
-}
-
-window.onload = (event) => {
-  if (localStorage.getItem("Badeloft-Details") == null) {
-    window.location.replace("login.html");
-  }
-  else {
-    var par = JSON.parse(localStorage.getItem('Badeloft-Details'));
-    var u = par[0].username;
-    var uname = document.getElementById("username");
-    uname.innerHTML = u;
-  }
-};
-
-function SecondPartDetails() {
-  var Partnumber = document.getElementById('partNo').value;
-  var Ordered = document.getElementById('order').value;
-  var Notes = document.getElementById('notes').value;
-  var invoice = 15000;
-  if (SecondPartDetail == null) {
-    SecondPartDetail = [];
-  }
-  SecondPartDetail.push({
-    Partnumber: Partnumber,
-    invoice: invoice,
-    Ordered: Ordered,
-    Notes: Notes
-  });
-  console.log(Notes);
-  var html = "";
-  SecondPartDetail.forEach(function (element, index) {
-    let ind = index + 1;
-    html += "<tr>";
-    html += "<td>" + element.Partnumber + "</td>";
-    html += "<td>" + element.invoice + "</td>";
-    html += "<td>" + element.Ordered + "</td>";
-    html += "<td>" + element.Notes + "</td>";
-    html += "<td>" + '<i class="fa fa-times"></i>' + "</td>";
-    html += "<tr>";
-    document.getElementById("ModalData").innerHTML = html;
-
-  });
-  document.getElementById("partNo").value = "";
-  document.getElementById("order").value = "";
-  document.getElementById("notes").value = "";
-}
-
-
-function AddNewStock() {
-
-  const CreatedDate = new Date(Date.now()).toLocaleString().split(',')[0];
-  console.log("SecondPartDetails", SecondPartDetail);
-
-  var stockName = document.getElementById('StockName').value;
-  var EtaDate = document.getElementById('ETADate').value;
-  var SelectedRadio = document.forms.AddStockForm.StockStatus.value;
-  console.log(SelectedRadio);
-  var GetName = JSON.parse(localStorage.getItem('Badeloft-Details'));
-  var CreatedBy = GetName[0].username;
-  console.log(CreatedBy);
-  var Notes = "StockPart Add";
-
-  var stockObj = {
-    index: index,
-    stockName : stockName,
-    EtaDate :EtaDate,
-    SelectedRadio :SelectedRadio,
-    CreatedBy : CreatedBy,
-    CreatedDate: CreatedDate,
-    Notes:Notes,
-    SecondPartDetail:SecondPartDetail
-  }
-  index++;
-  console.log(index);
-  console.log("SecondPartDetail", SecondPartDetail)
-  var StockDetail = JSON.parse(localStorage.getItem("StockDetails")) || [];
-  StockDetail.push(stockObj);
-  localStorage.setItem("StockDetails", JSON.stringify(StockDetail));
-  console.log("StockDetails", StockDetail);
-  table.row.add(
-    {
-      "stockName": stockName,
-      "EtaDate": EtaDate,
-      "SelectedRadio": SelectedRadio,
-      "CreatedBy": CreatedBy,
-      "CreatedDate": CreatedDate,
-      "Notes": Notes,
-    }).draw();
-    // document.getElementById("StockName").value = "";
-    // document.getElementById("ETADate").value = "";
-    // document.getElementById("partNo").value = "";
-    // document.getElementById("order").value = "";
-    // document.getElementById("notes").value = "";
-  }
-// $(function() {
-//   $('input[name="ETADate"]').daterangepicker({
-//     singleDatePicker: true,
-//     showDropdowns: true,
-//     minYear: 1901,
-//     maxYear: parseInt(moment().format('YYYY'),10)
-//   })
-// });
-
-// $("#addRow").click(function () {
- 
-//       if ($("#txtId").val() == '') {
-//           addDataToLocal();
-//       } else {
-//           updateDataFromLocal();
-//       }
-//   })
 $(document).ready(function () {
-  var modal = document.getElementById("AddStock");
-  table = $('#Stock_Table').DataTable({
 
-    data: JSON.parse(localStorage.getItem("StockDetails")),
-    "dom": '<"toolbar">frtip',
-    bFilter: true,
-    bInfo: true,
-    responsive: true, 
-    fnInitComplete: function () {
-      $('div.toolbar').html('<h2>Stock</h2>');
-      $('#Stock_Table_filter').prepend(modal);
-    },
+  // $('#addpartdatais').click(function () {
+  //   $('#partform').valid() == true
+  // })
+  //part data popup validation
+  // $("#PartNumberForm").validate({
+  //   rules: {
+  //     partname: {
+  //       required: true,
+  //       validpartname: true
+  //     },
+  //     orderd: {
+  //       required: true,
+  //       validorderd: true
+  //     },
+  //   },
+  //   messages: {
+  //     partname: {
+  //       required: "Enter partnumber",
+  //       // validpartname:""
+  //     },
+  //     orderd: {
+  //       required: "Enter orderd",
+  //       // validorderd: ""
+  //     },
+  //   },
+  // });
+
+  $("#AddStock").click(function(){
+    document.getElementById("AddStockForm").reset();
+
+  });
+
+  todayDate = new Date(Date.now()).toLocaleString().split(',')[0];
+  stockList = JSON.parse(localStorage.getItem('stockList') || '[]');
+  console.log(stockList)
+
+  if (stockList == null) {
+    stockList = []
+    localStorage.setItem('stockList', stockList)
+  }
+  console.log(stockList);
+
+  // datatable code
+  var modal = document.getElementById("AddStock");
+ 
+  tableData = $('#Stock_Table').DataTable({
+   
+
     language: {
-      search: "_INPUT_",
-      // "search": '<i class="fa fa-search"></i>',
-      searchPlaceholder: 'Search...',
+
+      info: "Items _START_ to _END_ of _TOTAL_ total",
       paginate: {
         next: '&#62',
-        previous: '&#60'
+        previous: '&#60',
+        info: true
+    
       },
-      "info": "Items _START_ to _END_ of _TOTAL_ total"
-
+      searchPlaceholder: " Search here...",
+   
+   
     },
+
+    "dom": '<"toolbar">frtip',
+    bFilter: true, bInfo: true,
+    fnInitComplete: function () {
+      $('div.toolbar').html(' <h4>Stock</h4>');
+      $('#Stock_Table_filter').prepend(modal);
+    },
+
+    lengthChange: false,
+    info:true,
+    
     columns: [
+      {
+        className: ' toHide',
+        orderable: false,
+        data: null,
+        defaultContent: '',
+      },
 
-    { title: 'Stock Name', orderable: false, className: "text-start dt-control", data: "stockName" },
-    { title: 'ETA Date', orderable: false, className: "text-center", data: "EtaDate" },
-    { title: 'Stock Location', orderable: false, className: "text-center", data : "SelectedRadio"},
-    { title: 'Created By', orderable: false, className: "text-center",data : "CreatedBy" },
-    { title: 'Created Date', orderable: false, className: "text-center",data: "CreatedDate" },
-    {
+
+
+      { title: 'Stock Name', orderable: false, className: "text-left dt-control colSpan", data: "sname" },
+      { title: 'ETA Date', orderable: false, className: "text-center  ", data: "etadate" },
+      { title: 'Stock Location', orderable: false, className: "text-center", data: "SelectedRadio" },
+      {title: 'Created By', orderable: false, className: "text-center", data: "createdby"},
+      { title: 'Created Date', orderable: false, className: "text-center", data: "createdDate" },
+      // { title: 'Notes', orderable: false, className: "text-center", data: "notes" },
+      { title: 'Action', orderable: false, className: "text-center", 
       data: null,
-      title: 'Action',
-      orderable: false,
-      defaultContent: '<div class="action-buttons">' +
-        '<span  class="editStock" data-id="' + index + '"><i class="fas fa-pen"></i></span> ' +
-        '<span class="history">&nbsp;&nbsp;<i class="fas fa-history"></i></span> ' +
-        '</div>',
-      className: 'row-edit dt-center',
-    }
+      className: "dt-center editor-edit  history",
+      defaultContent: '<i class="fa fa-pencil"/> <i class="fa fa-history"/>',
+      orderable: false
+    },
+  
     ],
-    
+
+    order: [[1, 'asc']],
+    data: stockList
   });
 
-  // Edit Part
-  $('#Stock_Table tr').on( 'click', '.editStock', function () {
-    
-     $("#AddNewStock").modal("show");
-     var StockData = table.row( $(this).closest('tr')).data();
-  
-    document.getElementById("StockName").value = StockData.stockName;
-    document.getElementById("ETADate").value = StockData.EtaDate;
-    console.log()
-    var html = "";
-    
-    StockData.SecondPartDetail.forEach(function (element, index) {
-      html += "<tr>";
-      html += "<td>" + element.Partnumber + "</td>";
-      html += "<td>" + element.invoice + "</td>";
-      html += "<td>" + element.Ordered + "</td>";
-      html += "<td>" + element.Notes + "</td>";
-      html += "<td>" + '<i class="fa fa-times"></i>' + "</td>";
-      html += "</tr>";
-      document.getElementById("ModalData").innerHTML = html;
-    })
-});
-$('#SaveStock').on('click', function() {
-  debugger
-  var updatedStockName = document.getElementById("StockName").value;
-  console.log(updatedStockName);
-  var updatedETADate = document.getElementById("ETADate").value;
-  console.log(updatedETADate);
-  var updatedSecondPartDetail = [];
-  
-  $("#ModalData > tr").each(function() {
+  $('th.toHide').hide();
+  $('th.colSpan').attr("colspan", 2);
+
+
+  //Edit record
+  $('#Stock_Table tr').on('click', '.editor-edit', function (e) {
     debugger
-    var updatedPartnumber = $(this).find("td").eq(0).html();
-    var updatedInvoice = $(this).find("td").eq(1).html();
-    var updatedOrdered = $(this).find("td").eq(2).html();
-    var updatedNotes = $(this).find("td").eq(3).html();
-    updatedSecondPartDetail.push({
-      "Partnumber": updatedPartnumber,
-      "invoice": updatedInvoice,
-      "Ordered": updatedOrdered,
-      "Notes": updatedNotes
+    e.preventDefault();
+   partDetails=[];
+    
+    // var indexvalue = $(this).closest('tr').index();
+    datatableIndex = tableData.row($(this).parents('tr')).index()
+    console.log(datatableIndex)
+    var indexvalue = datatableIndex;
+
+    // $("#addstockdatais").hide();
+    // $("#AddNewStock").show();
+  
+    $("#AddNewStock").modal("show");
+    // $("#titleofmodel").html("Edit Stock");
+
+    
+    console.log(stockList[datatableIndex]);
+    // indexvalue = stockList[datatableIndex].index;
+  
+
+    document.getElementById("StockName").value = stockList[datatableIndex].sname;
+    document.getElementById("ETADate").value = stockList[datatableIndex].etadate;
+    
+    partDetails = stockList[datatableIndex].stockList;
+  
+    var html = "";
+  
+    stockList[indexvalue].partDetails.forEach(function (element, index) {
+      let ind = index + 1;
+      
+      html += "<tr>";
+      html += "<td>" + element.partno + "</td>";
+      html += "<td>" + ind + "</td>"; 
+  
+      html += "<td>" + element.ordered + "</td>";
+      html += "<td>" + element.notes + "</td>";
+      html += "<td>" + '<i class="fa-solid fa-xmark delete"></i>' + "</td>";
+  
+      html += "</tr>";
+  
+  
+      document.getElementById("tdata").innerHTML = html;
+  
     });
-  });
-  
-  var rowData = {
-    "stockName": updatedStockName,
-    "EtaDate": updatedETADate,
-    "SecondPartDetail": updatedSecondPartDetail
-  };
-  
-  var row = table.row($(this).closest('tr'));
-  row.data(rowData).draw();
-  
-  $("#AddNewStock").modal("hide");
-});
 
-  $('#AddPartNumber').click(function () {
-    $('#addpartModal').modal('show');
+    
+        
+        
   });
 
-  $('#AddStock').click(function () {
-    $('#AddNewStock').modal('show');
-  });
 
+
+  // Add event listener for opening and closing details
   $('#Stock_Table tbody').on('click', 'td.dt-control', function () {
     var tr = $(this).closest('tr');
-    var row = table.row(tr);
+    var row = tableData.row(tr);
 
     if (row.child.isShown()) {
+      // This row is already open - close it
       row.child.hide();
       tr.removeClass('shown');
     } else {
+      // Open this row
       row.child(format(row.data())).show();
       tr.addClass('shown');
     }
   });
+
+  //delete parttable row in array
+ 
+  // $("#tdata").on('click','.delete',function(){
+  //   var i = $(this).closest('tr').index();
+  //   console.log(i)
+  //   partDetails.splice(i,1); 
+  //   console.log(partDetails)
+    
+  //   $(this).closest('tr').remove();
+  // });
+
+
 });
+
+
+  function updatestockdata(){                                                                                       // update stock
+    
+      //Getting all stock details and part details
+      var stockname = $("#StockName").val();
+      var etadate = $("#ETADate").val();
+     
+    
+     //Getting username for created by
+     var GetName = JSON.parse(localStorage.getItem('Badeloft-Details'));
+     var createdby  = GetName[0].username;
+     console.log(createdby);
+    
+      //Getting current date
+      var createdDate = new Date(Date.now()).toLocaleString().split(',')[0];
+    
+      // var notes = partDetails[0].notes;
+    
+      //Getting Stock status
+      // var production = document.getElementById("onproduction");
+      // var water = document.getElementById("onwater");
+      // var warehouse = document.getElementById("onwarehouse");
+    
+      // var stkstatus = "";
+    
+      // if (production == true) {
+      //   stkstatus = "On Production";
+      // } else if (water == true) {
+      //   stkstatus = "On Water";
+      // } else if (warehouse == true) {
+      //   stkstatus = "In Warehouse";
+      // }
+
+    
+  var SelectedRadio = document.forms.AddStockForm.StockStatus.value;
+  console.log(SelectedRadio);
+      // var partno = document.getElementById("partno").value
+      // var ordered = document.getElementById("ordered").value
+      // var notes = document.getElementById("notes").value
+    
+    
+  
+     var updateddata ={
+      index: datatableIndex,
+      sname: stockname,
+      etadate: etadate,
+      SelectedRadio: SelectedRadio,
+      createdby: createdby,
+      createdDate: createdDate,
+  
+      partDetails: partDetails
+    }
+    stockList[datatableIndex] = updateddata
+    
+      localStorage.setItem("stockList", JSON.stringify(stockList));
+    
+   
+      delete updateddata['index'];
+  delete updateddata['partDetails'];
+      tableData.row(datatableIndex).data(updateddata).draw();
+      // tableData.row.add(updateddata).draw();
+    
+    
+    }
+
+
+
+function addpartData() {
+
+  
+
+
+
+
+  // var partDetails = [];
+  console.log(partDetails);
+  // var partDetails = localStorage.getItem("partDetails");
+ 
+
+
+  var partno = document.getElementById('partNo').value;
+  var ordered = document.getElementById('order').value;
+  var notes = document.getElementById('notes').value;
+
+  // var partDetails= JSON.parse(partDetails);
+  if (partDetails == null) {
+
+    partDetails = [];
+
+  }
+  partDetails.push({
+    partno: partno,
+    ordered: ordered,
+    notes: notes,
+  });
+  // localStorage.setItem("partDetails", JSON.stringify(partDetails));
+
+  var html = "";
+
+
+  partDetails.forEach(function (element, index) {
+    let ind = index + 1;
+
+    
+    // id='row" + id + "
+    html += "<tr>";
+    html += "<td>" + element.partno + "</td>";
+    html += "<td>" + ind + "</td>";
+   
+    html += "<td>" + element.ordered + "</td>";
+    html += "<td>" + element.notes + "</td>";
+    html += "<td>" + '<i class="fa fa-times delete aria-hidden="true"></i>'+ "</td>";
+    // html += "<td>" + '<button onclick="deleteRecord(" + id + ")">Remove</button> '+ "</td>";
+    
+    html += "</tr>";
+
+    document.getElementById("ModalData").innerHTML = html;
+    
+  });
+
+}
+
+
+function addstockdata() {
+
+  // var stockDetails = [];
+  console.log("Partdetails in stock function:", partDetails);
+
+  var GetName = JSON.parse(localStorage.getItem('Badeloft-Details'));
+  var createdby = GetName[0].username;
+  console.log(createdby);
+  var sname = document.getElementById('StockName').value;
+  var etadate = document.getElementById('ETADate').value;
+  var SelectedRadio = document.forms.AddStockForm.StockStatus.value;
+  console.log(SelectedRadio);
+  var createdby = createdby;
+  var notes = document.getElementById('notes').value;
+  console.log(notes);
+
+
+  // stockDetails.push({
+  //   sname: sname,
+  //   etadate: etadate,
+  //   stkstatus: stkstatus,
+  //   createdby: createdby,
+  //   notes: notes,
+  // });
+  console.log(stockList)
+  stockList.push({
+    "sname": sname,
+    "etadate": etadate,
+    "SelectedRadio": SelectedRadio,
+    "createdby": createdby,
+    "createdDate": todayDate,
+    "notes": notes,
+    "partDetails": partDetails
+
+  });
+  console.log(stockList)
+  tableData.row.add(
+    {
+      "sname": sname,
+      "etadate": etadate,
+      "SelectedRadio": SelectedRadio,
+      "createdby":createdby,
+      "createdDate": todayDate,
+      "notes": notes,
+    }
+  ).draw();
+  // tableData.row.add(['', sname, etadate, stkstatus, createdby, '', notes, '']).draw();
+
+  console.log("stockList", stockList)
+
+  localStorage.setItem("stockList", JSON.stringify(stockList));
+}
+//array data delete code
+// function deleteRecord(id) {
+//   document.querySelector('#row' + id).remove();
+//   const updatedArray = users.filter((user) => Number(user[0]) !== id);
+//   console.log(updatedArray);
+// }
+
