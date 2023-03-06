9931ddd495cc61b3828d6cccaf35c88d8f3ba77d
Alok pal <alokpal28797@gmail.com>
2023-03-06 18:58:47 +0530

    Badeloft completed with some bugs

--- Trainee2023/Alok Pal/BadeLoft/stock1.js
+++ Trainee2023/Alok Pal/BadeLoft/stock1.js
@@ -69,6 +69,7 @@ function format(d, id) {
 // parts new
 
 function modal() {
+  IsEditing = false;
   $("#StockModal").modal("show");
   parts = [];
 }
@@ -219,8 +220,10 @@ $(document).ready(function () {
 
 // Array creation
 var parts = new Array();
+var IsEditing = false;
 
 function getModal() {
+  debugger
   var partN = document.getElementById("pNum").value;
   var order = document.getElementById("order").value;
   var comments = document.getElementById("floatingTextarea").value;
@@ -251,46 +254,18 @@ var newStock = [];
 var Stockstatus;
 
 function addStockData() {
-  // parts
-  var stockName = document.getElementById("stock").value;
-  var dateM = document.getElementById("date").value;
-
-  Stockstatus = radioBtnValue();
-
-  console.log(parts.length);
-  if (stockName != null && parts.length > 0) {
-    if (localStorage.getItem("newStock") == null) {
-      newStock = [];
-      var newObj = {
-        stock: stockName,
-        date: dateM,
-        stockStatus: Stockstatus,
-        CreatedDate: today,
-        Part: parts,
-        user: user,
-      };
-      newStock.push(newObj);
-      table.row.add(newObj).draw();
-      localStorage.setItem("newStock", JSON.stringify(newStock));
-      $("#StockModal").modal("hide");
-      resetSVal();
-      parts = [];
-      $("#partTable").append("<tbody></tbody");
-    } else {
-      newStock = JSON.parse(localStorage.getItem("newStock"));
-
-      var flag;
-      for (let i = 0; i < newStock.length; i++) {
-        flag = false;
-        if (stockName == newStock[i].stock) {
-          flag = true;
-          break;
-        } else {
-          flag = false;
-        }
-      }
-
-      if (flag == false) {
+  if(!IsEditing){
+    debugger
+    // parts
+    var stockName = document.getElementById("stock").value;
+    var dateM = document.getElementById("date").value;
+  
+    Stockstatus = radioBtnValue();
+  
+    console.log(parts.length);
+    if (stockName != null && parts.length > 0) {
+      if (localStorage.getItem("newStock") == null) {
+        newStock = [];
         var newObj = {
           stock: stockName,
           date: dateM,
@@ -304,30 +279,62 @@ function addStockData() {
         localStorage.setItem("newStock", JSON.stringify(newStock));
         $("#StockModal").modal("hide");
         resetSVal();
-        parts = [];
-        showModaltable();
+        // parts = [];
+        $('#partTable').html(null);
         $("#partTable").append("<tbody></tbody");
+      } else {
+        newStock = JSON.parse(localStorage.getItem("newStock"));
+  
+        var flag;
+        for (let i = 0; i < newStock.length; i++) {
+          flag = false;
+          if (stockName == newStock[i].stock) {
+            flag = true;
+            break;
+          } else {
+            flag = false;
+          }
+        }
+  
+        if (flag == false) {
+          var newObj = {
+            stock: stockName,
+            date: dateM,
+            stockStatus: Stockstatus,
+            CreatedDate: today,
+            Part: parts,
+            user: user,
+          };
+          newStock.push(newObj);
+          table.row.add(newObj).draw();
+          localStorage.setItem("newStock", JSON.stringify(newStock));
+          $("#StockModal").modal("hide");
+          resetSVal();
+          // parts = [];
+          $('#partTable').html(null);
+          $("#partTable").append("<tbody></tbody");
+        } else {
+          Swal.fire({
+            title: "Error!",
+            text: "Duplicate Stock Name is not allowed",
+            icon: "error",
+          });
+        }
+      }
+    } else {
+      if (stockName == "") {
+        Swal.fire({
+          title: "Error!",
+          text: "Add Stock Name",
+          icon: "error",
+        });
       } else {
         Swal.fire({
           title: "Error!",
-          text: "Duplicate Stock Name is not allowed",
+          text: "Please add part",
           icon: "error",
         });
       }
-    }
-  } else {
-    if (stockName == "") {
-      Swal.fire({
-        title: "Error!",
-        text: "Add Stock Name",
-        icon: "error",
-      });
-    } else {
-      Swal.fire({
-        title: "Error!",
-        text: "Please add part",
-        icon: "error",
-      });
     }
   }
 }
@@ -336,6 +343,7 @@ function addStockData() {
 
 // show data
 function showModaltable() {
+  debugger
   var html = "";
   if (parts.length > 0) {
     html +=
@@ -395,7 +403,9 @@ $(function () {
   );
 });
 
-$("#stock_table").on("click", ".editor-edit", function (e) {
+$("#stock_table").on("click", ".editor-edit", function () {
+  debugger
+  IsEditing = true;
   $("#StockModal").modal("show");
 
   // var indexRow = table.row(this).index();
@@ -406,7 +416,7 @@ $("#stock_table").on("click", ".editor-edit", function (e) {
   var tabledata = table.row($(this).parents("tr")).data();
   var indexRow = table.row($(this).parents("tr")).index();
 
-  var newStockModal = JSON.parse(localStorage.getItem("newStock"));
+  var newStockModal = JSON.parse(localStorage.getItem("newStock"))|| [];
   console.log(newStockModal[indexRow].Part[0]);
 
   document.getElementById("stock").value = newStockModal[indexRow].stock;
@@ -417,29 +427,33 @@ $("#stock_table").on("click", ".editor-edit", function (e) {
     true
   );
 
-  showModaltable();
+  
   parts = newStockModal[indexRow].Part;
+  showModaltable();
   document.querySelector("#modalstock").onclick = function () {
-    var updatestock = (newStockModal[indexRow].stock =
-      document.getElementById("stock").value);
-    var updatedate = (newStockModal[indexRow].date =
-      document.getElementById("date").value);
-
-    Stockstatus = radioBtnValue();
-
-    var newObj = {
-      stock: updatestock,
-      date: updatedate,
-      stockStatus: Stockstatus,
-      CreatedDate: today,
-      Part: parts,
-      user: user,
-    };
-
-    newStock[indexRow] = newObj;
-    localStorage.setItem("newStock", JSON.stringify(newStock));
-    $("#StockModal").modal("hide");
-    window.location.reload();
+    if(IsEditing){
+      debugger
+      var updatestock = (newStockModal[indexRow].stock =
+        document.getElementById("stock").value);
+      var updatedate = (newStockModal[indexRow].date =
+        document.getElementById("date").value);
+  
+      Stockstatus = radioBtnValue();
+  
+      var newObj = {
+        stock: updatestock,
+        date: updatedate,
+        stockStatus: Stockstatus,
+        CreatedDate: today,
+        Part: parts,
+        user: user,
+      };
+  
+      newStockModal[indexRow] = newObj;
+      localStorage.setItem("newStock", JSON.stringify(newStockModal));
+      $("#StockModal").modal("hide");
+      window.location.reload();
+    }
   };
 });
 
