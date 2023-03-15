104abd9c5bc7d764c7af204e92f2f6cc61d13bbd
mahediali momin <mahedialimomin17@gmail.com>
2023-03-15 14:26:46 +0530

    all changes done

--- Trainee2023/Mahediali Momin/JAVASCRIPT/JAVASCRIPT_EXAM/Standardizer/StandardizerCOA.js
+++ Trainee2023/Mahediali Momin/JAVASCRIPT/JAVASCRIPT_EXAM/Standardizer/StandardizerCOA.js
@@ -11,40 +11,40 @@ request.responseType = 'text';
 
 // set a callback function to handle the file data once it's loaded
 request.onload = function () {
-    // check that the request was successful
-    if (request.status === 200) {
-        // split the file contents into an array of lines
-        var lines = request.response.split('\n');
+  // check that the request was successful
+  if (request.status === 200) {
+    // split the file contents into an array of lines
+    var lines = request.response.split('\n');
 
-        // remove the first line (header) of the file
-        var headers = lines.shift().split(',');
+    // remove the first line (header) of the file
+    var headers = lines.shift().split(',');
 
-        // map the remaining lines into an array of objects
-        data = lines.map(function (line) {
-            var values = line.split(',');
-            var object = {};
+    // map the remaining lines into an array of objects
+    data = lines.map(function (line) {
+      var values = line.split(',');
+      var object = {};
 
-            // loop through all the columns in the row
-            for (var i = 0; i < headers.length; i++) {
-                // set the key-value pair in the object
-                object[headers[i]] = values[i];
-            }
-            return object;
+      // loop through all the columns in the row
+      for (var i = 0; i < headers.length; i++) {
+        // set the key-value pair in the object
+        object[headers[i]] = values[i];
+      }
+      return object;
 
-        });
+    });
 
-        // do something with the data
-        console.log(data);
-        // create a new ul element
-        var ul = document.getElementById("section2");
-        for (var i = 0; i < data.length; i++) {
-            var li = document.createElement("li");
-            li.className = "list-group-item border";
-            li.innerHTML = '<i class="fa-regular fa-grip-dots-vertical"></i> ' + data[i]["AccountCode"] + " -- " + data[i]["AccountName"];
+    // do something with the data
+    console.log(data);
+    // create a new ul element
+    var ul = document.getElementById("section2");
+    for (var i = 0; i < data.length; i++) {
+      var li = document.createElement("li");
+      li.className = "list-group-item border";
+      li.innerHTML = '<i class="fa-regular fa-grip-dots-vertical"></i> ' + data[i]["AccountCode"] + " -- " + data[i]["AccountName"];
 
-            ul.appendChild(li);
-        }
+      ul.appendChild(li);
     }
+  }
 };
 // send the request
 request.send();
@@ -59,111 +59,290 @@ request1.responseType = 'text';
 
 // set a callback function to handle the file data once it's loaded
 request1.onload = function () {
-    // check that the request was successful
-    if (request1.status === 200) {
-        // split the file contents into an array of lines
-        var lines = request1.response.split('\n');
+  // check that the request was successful
+  if (request1.status === 200) {
+    // split the file contents into an array of lines
+    var lines = request1.response.split('\n');
 
-        // remove the first line (header) of the file
-        var headers = lines.shift().split(',');
+    // remove the first line (header) of the file
+    var headers = lines.shift().split(',');
 
-        // map the remaining lines into an array of objects
-        var data = lines.map(function (line) {
-            var values = line.split(',');
-            var object = {};
+    // map the remaining lines into an array of objects
+    data = lines.map(function (line) {
+      var values = line.split(',');
+      var object = {};
 
-            // loop through all the columns in the row
-            for (var i = 0; i < headers.length; i++) {
-                // set the key-value pair in the object
-                object[headers[i]] = values[i];
-            }
-            return object;
-        });
+      // loop through all the columns in the row
+      for (var i = 0; i < headers.length; i++) {
+        // set the key-value pair in the object
+        object[headers[i]] = values[i];
+      }
+      return object;
+    });
 
-        // do something with the data
-        console.log("DATA", data);
-
-        // create a new ul element
-        var ul = document.getElementById("section1");
-
-        for (var i = 0; i < data.length; i++) {
-            var li = document.createElement("li");
-            li.className = "list-group-item border";
-            li.innerHTML = data[i]["Number"] + " -- " + data[i]["Name"];
-            ul.appendChild(li);
-        }
+    // create a new ul element
+    var ul1 = document.getElementById("section1");
+    
+    for (var i = 0; i < data.length; i++) {
+      if(data[i]["Number"]!=""){
+      var li = document.createElement("li");
+      li.className = "list-group-item border";
+      li.innerHTML = data[i]["Number"] + " -- " + data[i]["Name"];
+      ul1.appendChild(li);}
     }
+  }
 };
 // send the request
 request1.send();
 
 
 
+let parsedData = [];
 
-$(".header-button").click(function () {
-    var btnType = $(this).data("btn-type"); // Get the data attribute of the button that was clicked
-    
-    // Clear both sections and set up new lists
-    var ul1 = document.getElementById("section1");
-    ul1.innerHTML = "";
-    
-    var ul2 = document.getElementById("section2");
-    ul2.innerHTML = "";
+function parseCSVData() {
+  var request1 = new XMLHttpRequest();
+  // GET and the file URL
+  request1.open('GET', 'Standard CofA.csv', true);
 
-    var filteredData1 = data.filter(function(item) {
-        switch (btnType) {
-          case "assets":
-            return item.Type === "Assets";
-          case "liability":
-            return item.Type === "Liabilities";
-          case "equity":
-            return item.Type === "Equity";
-          case "revenue":
-            return item.Type === "Revenue";
-          case "cogs":
-            return item.Type === "COGS";
-          case "expenses":
-            return item.Type === "Expense";
-          case "other":
-            return item.Type === "Other Rev & Exp";
+  // set the responseType to text to receive the file contents as a string
+  request1.responseType = 'text';
+
+  // set a callback function to handle the file data once it's loaded
+  request1.onload = function () {
+    // check that the request was successful
+    if (request1.status === 200) {
+      // split the file contents into an array of lines
+      var lines = request1.response.split('\n');
+
+      // remove the first line (header) of the file
+      var headers = lines.shift().split(',');
+
+      // map the remaining lines into an array of objects
+      parsedData = lines.map(function (line) {
+        var values = line.split(',');
+        var object = {};
+
+        // loop through all the columns in the row
+        for (var i = 0; i < headers.length; i++) {
+          // set the key-value pair in the object
+          object[headers[i]] = values[i];
         }
+        return object;
       });
 
-    var filteredData2 = data.filter(function(item) {
-        switch (btnType) {
-          case "assets":
-            return item.AccountTypeName === "ASSETS";
-          case "liability":
-            return item.AccountTypeName === "LIABILITIES";
-          case "equity":
-            return item.AccountTypeName === "EQUITY/CAPITAL";
-          case "revenue":
-            return item.AccountTypeName === "Professional Services Revenue";
-          case "cogs":
-            return item.AccountTypeName === "";
-          case "expenses":
-            return item.AccountTypeName === "Labor Expense";
-          case "other":
-            return item.AccountTypeName === "Product Costs";
+      // do something with the parsed data, if needed
+      console.log("Parsed data:", parsedData);
+    }
+  };
+
+  // send the request
+  request1.send();
+}
+
+// call the parseCSVData function once on page load
+parseCSVData();
+
+
+
+
+const buttons = document.querySelectorAll('.header-button');
+const links = document.querySelectorAll('.menu-inner-box a');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    buttons.forEach(button => {
+      button.classList.remove('active');
+    });
+    button.classList.add('active');
+
+    const btnType = button.getAttribute('data-btn-type');
+    links.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('data-btn-type') === btnType) {
+        link.classList.add('active');
+        if (link.classList.contains('active')) {
+          link.click();
         }
-      });
+      }
+    });
 
-      
-      filteredData1.forEach(function(item) {
-        var li = document.createElement("li");
-        li.className = "list-group-item border";
-        li.innerHTML = item["Number"] + " -- " + item["Name"];
-        ul1.appendChild(li);
-      });
+    // filter the parsedData array to only show objects with a "Type" key that matches the button's custom attribute
+    const filteredData = parsedData.filter(obj => obj.Type === btnType);
 
-      filteredData2.forEach(function(item) {
-        var li = document.createElement("li");
-        li.className = "list-group-item border";
-        li.innerHTML = item["AccountCode"] + " -- " + item["AccountName"];
-        ul2.appendChild(li);
-      });
-    
+    // update the list to display the name and number of each matching object
+    const ul1 = document.getElementById("section1");
+    ul1.innerHTML = '';
+    filteredData.forEach(obj => {
+      if(obj.Number!=""){
+      var li = document.createElement("li");
+      li.className = "list-group-item border";
+      li.innerHTML = obj.Number + " -- " + obj.Name;
+      ul1.appendChild(li);
+      }
+    });
+  });
 });
+
+
+
+// When a menu link is clicked
+$('.menu-inner-box a').click(function () {
+  // Remove the "active" class from all links
+  $('.menu-inner-box a').removeClass('active');
+  // Add the "active" class to the clicked link
+  $(this).addClass('active');
+
+  var linkValue = $(this).attr('data-link-value').toLowerCase();
+
+  var request = new XMLHttpRequest();
+
+  // GET and the file URL
+  request.open('GET', 'MasterChartOfAcounts - Sheet1.csv', true);
+
+  // set the responseType to text to receive the file contents as a string
+  request.responseType = 'text';
+
+  // set a callback function to handle the file data once it's loaded
+  request.onload = function () {
+    // check that the request was successful
+    if (request.status === 200) {
+      // split the file contents into an array of lines
+      var lines = request.response.split('\n');
+
+      // remove the first line (header) of the file
+      var headers = lines.shift().split(',');
+
+      // map the remaining lines into an array of objects
+      data = lines.map(function (line) {
+        var values = line.split(',');
+        var object = {};
+
+        // loop through all the columns in the row
+        for (var i = 0; i < headers.length; i++) {
+          // set the key-value pair in the object
+          object[headers[i]] = values[i];
+        }
+        return object;
+
+      });
+      // create a new ul element
+      var ul = document.getElementById("section2");
+      ul.innerHTML = '';
+      for (var i = 0; i < data.length; i++) {
+        if (((data[i].AccountTypeName).toLowerCase()).includes(linkValue)) {
+          var li = document.createElement("li");
+          li.className = "list-group-item border";
+          li.innerHTML = '<i class="fa-regular fa-grip-dots-vertical"></i> ' + data[i].AccountCode + " -- " + data[i].AccountName;
+          ul.appendChild(li);
+        }
+      }
+    }
+  };
+  // send the request
+  request.send();
+});
+
+
+
+// select the "mostlikely" list
+var mostlikelyList = $("#mostlikely");
+var mostlikelyList = $("#likely");
+var mostlikelyList = $("#possible");
+
+// create a new list item with some text
+var newItem = $("<li>").text("");
+
+// append the new item to the list
+mostlikelyList.append(newItem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".header-button").click(function () {
+//     var btnType = $(this).data("btn-type"); // Get the data attribute of the button that was clicked
+
+//     // Clear both sections and set up new lists
+//     var ul1 = document.getElementById("section1");
+//     ul1.innerHTML = "";
+
+//     var ul2 = document.getElementById("section2");
+//     ul2.innerHTML = "";
+
+//     var filteredData1 = data.filter(function(item) {
+//         switch (btnType) {
+//           case "assets":
+//             return item.Type === "Assets";
+//           case "liability":
+//             return item.Type === "Liabilities";
+//           case "equity":
+//             return item.Type === "Equity";
+//           case "revenue":
+//             return item.Type === "Revenue";
+//           case "cogs":
+//             return item.Type === "COGS";
+//           case "expenses":
+//             return item.Type === "Expense";
+//           case "other":
+//             return item.Type === "Other Rev & Exp";
+//         }
+//       });
+
+//     var filteredData2 = data.filter(function(item) {
+//         switch (btnType) {
+//           case "assets":
+//             return item.AccountTypeName === "ASSETS";
+//           case "liability":
+//             return item.AccountTypeName === "LIABILITIES";
+//           case "equity":
+//             return item.AccountTypeName === "EQUITY/CAPITAL";
+//           case "revenue":
+//             return item.AccountTypeName === "Professional Services Revenue";
+//           case "cogs":
+//             return item.AccountTypeName === "";
+//           case "expenses":
+//             return item.AccountTypeName === "Labor Expense";
+//           case "other":
+//             return item.AccountTypeName === "Product Costs";
+//         }
+//       });
+
+
+//       filteredData1.forEach(function(item) {
+//         var li = document.createElement("li");
+//         li.className = "list-group-item border";
+//         li.innerHTML = item["Number"] + " -- " + item["Name"];
+//         ul1.appendChild(li);
+//       });
+
+//       filteredData2.forEach(function(item) {
+//         var li = document.createElement("li");
+//         li.className = "list-group-item border";
+//         li.innerHTML = item["AccountCode"] + " -- " + item["AccountName"];
+//         ul2.appendChild(li);
+//       });
+
+// });
 
 
 
@@ -176,7 +355,7 @@ $(".header-button").click(function () {
 //     // Clear section 2 and set up a new list
 //     var ul = document.getElementById("section2");
 //     ul.innerHTML = "";
-    
+
 //     var filteredData = data.filter(function(item) {
 //         switch (btnType) {
 //           case "assets":
@@ -195,17 +374,17 @@ $(".header-button").click(function () {
 //             return item.AccountTypeName === "Product Costs";
 //         }
 //       });
-      
+
 //     //   var ul = document.getElementById("section2");
 //     //   ul.innerHTML = "";
-      
+
 //       filteredData.forEach(function(item) {
 //         var li = document.createElement("li");
 //         li.className = "list-group-item border";
 //         li.innerHTML = item["AccountCode"] + " -- " + item["AccountName"];
 //         ul.appendChild(li);
 //       });
-    
+
 // });
 
 
@@ -234,83 +413,83 @@ $(".header-button").click(function () {
 //             return item.Type === "Other Rev & Exp";
 //         }
 //       });
-      
+
 //     //   var ul = document.getElementById("section1");
 //     //   ul.innerHTML = "";
-      
+
 //       filteredData.forEach(function(item) {
 //         var li = document.createElement("li");
 //         li.className = "list-group-item border";
 //         li.innerHTML = item["Number"] + " -- " + item["Name"];
 //         ul.appendChild(li);
 //       });
-    
+
 // });
 
 
 //for searchbar
 $(document).ready(function () {
-    $('.search').on('keyup search', function () {
-        var query = $(this).val().toLowerCase();
-        $('#section2 li').each(function () {
-            var text = $(this).text().toLowerCase();
-            if (text.indexOf(query) === -1) {
-                $(this).hide();
-            } else {
-                $(this).show();
-            }
-        });
+  $('.search').on('keyup search', function () {
+    var query = $(this).val().toLowerCase();
+    $('#section2 li').each(function () {
+      var text = $(this).text().toLowerCase();
+      if (text.indexOf(query) === -1) {
+        $(this).hide();
+      } else {
+        $(this).show();
+      }
     });
-    $('.search').on('search', function () {
-        if ($(this).val() === '') {
-            $('#section2 li').show();
-        }
-    });
+  });
+  $('.search').on('search', function () {
+    if ($(this).val() === '') {
+      $('#section2 li').show();
+    }
+  });
 });
 
 
 
 // header buttons
 $(document).ready(function () {
-    // Add click event listener to all buttons
-    $('.header-button').on('click', function () {
-        // Remove active class from all buttons
-        $('.header-button').removeClass('active');
-        // Add active class to clicked button
-        $(this).addClass('active');
-    });
+  // Add click event listener to all buttons
+  $('.header-button').on('click', function () {
+    // Remove active class from all buttons
+    $('.header-button').removeClass('active');
+    // Add active class to clicked button
+    $(this).addClass('active');
+  });
 });
 
 
 document.querySelectorAll('.menu-inner-box a').forEach(function (link) {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        document.querySelectorAll('.menu-inner-box a').forEach(function (link) {
-            link.classList.remove('active');
-        });
-        this.classList.add('active');
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    document.querySelectorAll('.menu-inner-box a').forEach(function (link) {
+      link.classList.remove('active');
     });
+    this.classList.add('active');
+  });
 });
 
 
 dragula([document.getElementById('section1'), document.getElementById('section2')], {
-    copy: function (el, source) {
-        return source === document.getElementById('section1')
-    },
-    accepts: function (el, target) {
-        return target !== document.getElementById('section1')
-    }
+  copy: function (el, source) {
+    return source === document.getElementById('section1')
+  },
+  accepts: function (el, target) {
+    return target !== document.getElementById('section1')
+  }
 });
 
 
 $(document).ready(function () {
-    $('#btn-nav-previous').click(function () {
-        $(".menu-inner-box").animate({ scrollLeft: "-=100px" });
-    });
+  $('#btn-nav-previous').click(function () {
+    $(".menu-inner-box").animate({ scrollLeft: "-=100px" });
+  });
 
-    $('#btn-nav-next').click(function () {
-        $(".menu-inner-box").animate({ scrollLeft: "+=100px" });
-    });
+  $('#btn-nav-next').click(function () {
+    $(".menu-inner-box").animate({ scrollLeft: "+=100px" });
+  });
 });
 
 // // Click event for all the buttons
